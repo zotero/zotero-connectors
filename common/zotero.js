203bd266f4b30c99464a21a51033b0d0759e6317
Simon Kornblith <simon@simonster.com>
2011-08-16 19:17:22 +0000

    Fix OAUTH_NEW_KEY_URL, in case we ever decide to re-enable OAuth superpowers

--- common/zotero.js
+++ common/zotero.js
@@ -35,7 +35,7 @@ const ZOTERO_CONFIG = {
 	OAUTH_ACCESS_URL: 'https://www.zotero.org/oauth/access',
 	OAUTH_AUTHORIZE_URL: 'https://www.zotero.org/oauth/authorize',
 	OAUTH_CALLBACK_URL: 'https://www.zotero.org/connector_auth_complete',
-	OAUTH_NEW_KEY_URL: 'https://www.zotero.org/settings/keys/new?oauth=1&oauth_consumer_key=91a6767411840e7fb6ed',
+	OAUTH_NEW_KEY_URL: 'https://www.zotero.org/settings/keys/new?oauth=1&oauth_consumer_key=05a4e25d3d9af8922eb9',
 	OAUTH_CLIENT_KEY: '05a4e25d3d9af8922eb9',
 	OAUTH_CLIENT_SECRET: '8dda1d6aa188bdd3126e'
 };

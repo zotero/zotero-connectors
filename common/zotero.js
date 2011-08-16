b65a2c21d2609b1be3e49fd0f309522a87d61a7c
Simon Kornblith <simon@simonster.com>
2011-08-16 05:03:33 +0000

    Closes #1847, Make connector_auth_complete page
    Uses https://www.zotero.org/connector_auth_complete as callback URL. Made a new key too, so that we don't kill the previous connector builds.

--- common/zotero.js
+++ common/zotero.js
@@ -34,10 +34,10 @@ const ZOTERO_CONFIG = {
 	OAUTH_REQUEST_URL: 'https://www.zotero.org/oauth/request',
 	OAUTH_ACCESS_URL: 'https://www.zotero.org/oauth/access',
 	OAUTH_AUTHORIZE_URL: 'https://www.zotero.org/oauth/authorize',
-	OAUTH_CALLBACK_URL: 'http://www.zotero.org/support/connector_auth_complete',
+	OAUTH_CALLBACK_URL: 'https://www.zotero.org/connector_auth_complete',
 	OAUTH_NEW_KEY_URL: 'https://www.zotero.org/settings/keys/new?oauth=1&oauth_consumer_key=91a6767411840e7fb6ed',
-	OAUTH_CLIENT_KEY: '91a6767411840e7fb6ed',
-	OAUTH_CLIENT_SECRET: '9ec47d3f24d6dff3bdaa'
+	OAUTH_CLIENT_KEY: '05a4e25d3d9af8922eb9',
+	OAUTH_CLIENT_SECRET: '8dda1d6aa188bdd3126e'
 };
 
 var Zotero = new function() {

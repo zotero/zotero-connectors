60caec8cae6a09175a738a4db600df4c8b6a02dc
Simon Kornblith <simon@simonster.com>
2012-03-06 01:21:32 -0500

    Package output properly

--- bookmarklet/tests/src/org/zotero/BookmarkletTester/TestResults.java
+++ bookmarklet/tests/src/org/zotero/BookmarkletTester/TestResults.java
@@ -3,16 +3,16 @@ package org.zotero.BookmarkletTester;
 import java.util.LinkedList;
 
 class TestResults {
-	LinkedList<TranslatorTester> translatorTesters;
+	LinkedList<TranslatorTester> results;
 	String browser, version;
 	
 	TestResults(String aBrowser, String aVersion, LinkedList<TranslatorTester> aTranslatorTesters) {
 		browser = aBrowser;
 		version = aVersion;
-		translatorTesters = aTranslatorTesters;
+		results = aTranslatorTesters;
 	}
 	
 	public String getBrowser() { return browser; };
 	public String getVersion() { return version; };
-	public LinkedList<TranslatorTester> getTranslatorTesters() { return translatorTesters; };
+	public LinkedList<TranslatorTester> getResults() { return results; };
 }

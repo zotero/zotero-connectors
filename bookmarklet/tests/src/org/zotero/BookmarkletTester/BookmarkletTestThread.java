db3ea3d172d594560507716bdfa4589d6c4f0943
Simon Kornblith <simon@simonster.com>
2012-03-20 20:21:40 -0400

    Restart IE driver every 20 translators, because it leaks like crazy

--- bookmarklet/tests/src/org/zotero/BookmarkletTester/BookmarkletTestThread.java
+++ bookmarklet/tests/src/org/zotero/BookmarkletTester/BookmarkletTestThread.java
@@ -10,58 +10,68 @@ import org.openqa.selenium.iphone.IPhoneDriver;
 
 public class BookmarkletTestThread extends Thread {
 	public void run() {
-		WebDriver driver;
-		boolean useTimeoutThread = false;
-		if(BookmarkletTester.config.browser.equals("g")) {
-			FirefoxProfile profile = new FirefoxProfile();
-			profile.setPreference("permissions.default.stylesheet", 2);
-			profile.setPreference("permissions.default.image", 2);
-			
-			driver = new FirefoxDriver();
-			driver.manage().timeouts().pageLoadTimeout(600, java.util.concurrent.TimeUnit.SECONDS);
-		} else if(BookmarkletTester.config.browser.equals("c")) {
-			useTimeoutThread = true;
-			driver = new ChromeDriver();
-		} else if(BookmarkletTester.config.browser.equals("i")) {
-			useTimeoutThread = true;
-			driver = new InternetExplorerDriver();
-		} else if(BookmarkletTester.config.browser.equals("p")) {
-			try {
-				driver = new IPhoneDriver();
-			} catch(Exception e) {
-				System.err.println("iPhone driver not available");
-				e.printStackTrace();
+		running: while(true) {
+			WebDriver driver;
+			boolean useTimeoutThread = false;
+			if(BookmarkletTester.config.browser.equals("g")) {
+				FirefoxProfile profile = new FirefoxProfile();
+				profile.setPreference("permissions.default.stylesheet", 2);
+				profile.setPreference("permissions.default.image", 2);
+				
+				driver = new FirefoxDriver();
+				driver.manage().timeouts().pageLoadTimeout(600, java.util.concurrent.TimeUnit.SECONDS);
+			} else if(BookmarkletTester.config.browser.equals("c")) {
+				useTimeoutThread = true;
+				driver = new ChromeDriver();
+			} else if(BookmarkletTester.config.browser.equals("i")) {
+				useTimeoutThread = true;
+				driver = new InternetExplorerDriver();
+			} else if(BookmarkletTester.config.browser.equals("p")) {
+				try {
+					driver = new IPhoneDriver();
+				} catch(Exception e) {
+					System.err.println("iPhone driver not available");
+					e.printStackTrace();
+					System.exit(1);
+					return;
+				}
+			} else if(BookmarkletTester.config.browser.equals("a")) {
+				driver = new AndroidDriver();
+			} else {
+				System.out.println("Unknown browser "+BookmarkletTester.config.browser);
 				System.exit(1);
 				return;
 			}
-		} else if(BookmarkletTester.config.browser.equals("a")) {
-			driver = new AndroidDriver();
-		} else {
-			System.out.println("Unknown browser "+BookmarkletTester.config.browser);
-			System.exit(1);
+			
+			driver.manage().timeouts().setScriptTimeout(180, java.util.concurrent.TimeUnit.SECONDS);
+			
+			TranslatorTester translatorTester;
+			int nTranslatorsTested = 0;
+			while((translatorTester = BookmarkletTester.getNextTranslatorTester()) != null) {
+				// Don't try to translate with excluded translators
+				if(BookmarkletTester.config.exclude.contains(translatorTester.translator.translatorID)) continue;
+	
+				TestTimeoutThread timeoutThread = null;
+				if(useTimeoutThread) {
+					timeoutThread = new TestTimeoutThread(600, this);
+					timeoutThread.start();
+				}
+				
+				translatorTester.runTests(driver);
+				
+				if(timeoutThread != null) {
+					timeoutThread.interrupt();
+				}
+				
+				if((++nTranslatorsTested) == 20 && BookmarkletTester.config.browser.equals("i")) {
+					// Internet Explorer leaks memory like a sieve
+					driver.quit();
+					continue running;
+				}
+			}
+			
+			driver.quit();
 			return;
 		}
-		
-		driver.manage().timeouts().setScriptTimeout(180, java.util.concurrent.TimeUnit.SECONDS);
-		
-		TranslatorTester translatorTester;
-		while((translatorTester = BookmarkletTester.getNextTranslatorTester()) != null) {
-			// Don't try to translate with excluded translators
-			if(BookmarkletTester.config.exclude.contains(translatorTester.translator.translatorID)) continue;
-
-			TestTimeoutThread timeoutThread = null;
-			if(useTimeoutThread) {
-				timeoutThread = new TestTimeoutThread(600, this);
-				timeoutThread.start();
-			}
-			
-			translatorTester.runTests(driver);
-			
-			if(timeoutThread != null) {
-				timeoutThread.interrupt();
-			}
-		}
-		
-		driver.quit();
 	}
 }

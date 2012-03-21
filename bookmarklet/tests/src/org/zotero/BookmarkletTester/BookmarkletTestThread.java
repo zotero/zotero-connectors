7d29f28ec71670369b44273371a5e1d22aed7942
Simon Kornblith <simon@simonster.com>
2012-03-20 23:33:35 -0400

    More timeout testing

--- bookmarklet/tests/src/org/zotero/BookmarkletTester/BookmarkletTestThread.java
+++ bookmarklet/tests/src/org/zotero/BookmarkletTester/BookmarkletTestThread.java
@@ -9,72 +9,57 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.iphone.IPhoneDriver;
 
 public class BookmarkletTestThread extends Thread {
-	public void run() {
-		running: while(true) {
-			WebDriver driver;
-			boolean useTimeoutThread = false;
-			if(BookmarkletTester.config.browser.equals("g")) {
-				FirefoxProfile profile = new FirefoxProfile();
-				profile.setPreference("permissions.default.stylesheet", 2);
-				profile.setPreference("permissions.default.image", 2);
-				
-				driver = new FirefoxDriver();
-				driver.manage().timeouts().pageLoadTimeout(30, java.util.concurrent.TimeUnit.SECONDS);
-			} else if(BookmarkletTester.config.browser.equals("c")) {
-				useTimeoutThread = true;
-				driver = new ChromeDriver();
-			} else if(BookmarkletTester.config.browser.equals("i")) {
-				System.setProperty("sun.net.client.defaultReadTimeout", "30000");
-				System.setProperty("sun.net.client.defaultConnectTimeout", "30000");
-				
-				useTimeoutThread = true;
-				driver = new InternetExplorerDriver();
-			} else if(BookmarkletTester.config.browser.equals("p")) {
-				try {
-					driver = new IPhoneDriver();
-				} catch(Exception e) {
-					System.err.println("iPhone driver not available");
-					e.printStackTrace();
-					System.exit(1);
-					return;
-				}
-			} else if(BookmarkletTester.config.browser.equals("a")) {
-				driver = new AndroidDriver();
-			} else {
-				System.out.println("Unknown browser "+BookmarkletTester.config.browser);
+	WebDriver driver;
+	
+	public void setupDriver() {
+		if(BookmarkletTester.config.browser.equals("g")) {
+			FirefoxProfile profile = new FirefoxProfile();
+			profile.setPreference("permissions.default.stylesheet", 2);
+			profile.setPreference("permissions.default.image", 2);
+			
+			driver = new FirefoxDriver();
+			driver.manage().timeouts().pageLoadTimeout(600, java.util.concurrent.TimeUnit.SECONDS);
+		} else if(BookmarkletTester.config.browser.equals("c")) {
+			driver = new ChromeDriver();
+		} else if(BookmarkletTester.config.browser.equals("i")) {
+			driver = new InternetExplorerDriver();
+		} else if(BookmarkletTester.config.browser.equals("p")) {
+			try {
+				driver = new IPhoneDriver();
+			} catch(Exception e) {
+				System.err.println("iPhone driver not available");
+				e.printStackTrace();
 				System.exit(1);
 				return;
 			}
-			
-			driver.manage().timeouts().setScriptTimeout(180, java.util.concurrent.TimeUnit.SECONDS);
-			
-			TranslatorTester translatorTester;
-			int nTranslatorsTested = 0;
-			while((translatorTester = BookmarkletTester.getNextTranslatorTester()) != null) {
-				// Don't try to translate with excluded translators
-				if(BookmarkletTester.config.exclude.contains(translatorTester.translator.translatorID)) continue;
-	
-				TestTimeoutThread timeoutThread = null;
-				if(useTimeoutThread) {
-					timeoutThread = new TestTimeoutThread(600, this);
-					timeoutThread.start();
-				}
-				
-				translatorTester.runTests(driver);
-				
-				if(timeoutThread != null) {
-					timeoutThread.interrupt();
-				}
-				
-				if((++nTranslatorsTested) == 40 && BookmarkletTester.config.browser.equals("i")) {
-					// Internet Explorer leaks memory like a sieve
-					driver.quit();
-					continue running;
-				}
-			}
-			
-			driver.quit();
+		} else if(BookmarkletTester.config.browser.equals("a")) {
+			driver = new AndroidDriver();
+		} else {
+			System.out.println("Unknown browser "+BookmarkletTester.config.browser);
+			System.exit(1);
 			return;
 		}
+		
+		driver.manage().timeouts().setScriptTimeout(180, java.util.concurrent.TimeUnit.SECONDS);
+	}
+	
+	public void run() {
+		TranslatorTester translatorTester;
+		int nTranslatorsTested = 0;
+		while((translatorTester = BookmarkletTester.getNextTranslatorTester()) != null) {
+			// Don't try to translate with excluded translators
+			if(BookmarkletTester.config.exclude.contains(translatorTester.translator.translatorID)) continue;
+			
+			translatorTester.runTests(this);
+			
+			if((++nTranslatorsTested) == 40 && BookmarkletTester.config.browser.equals("i")) {
+				// Internet Explorer leaks memory like a sieve
+				driver.quit();
+				setupDriver();
+			}
+		}
+		
+		driver.quit();
+		return;
 	}
 }
--- bookmarklet/tests/src/org/zotero/BookmarkletTester/TestTimeoutThread.java
+++ bookmarklet/tests/src/org/zotero/BookmarkletTester/TestTimeoutThread.java
@@ -1,19 +1,21 @@
 package org.zotero.BookmarkletTester;
 
+import org.openqa.selenium.WebDriver;
+
 public class TestTimeoutThread extends Thread {
 	final private int timeout;
-	final private Thread watchingThread;
+	final private WebDriver driver;
+	volatile boolean timedOut = false; 
 
-	public TestTimeoutThread(int seconds, Thread watchingThread) {
+	public TestTimeoutThread(int seconds, WebDriver driver) {
 		this.timeout = seconds;
-		this.watchingThread = watchingThread;
+		this.driver = driver;
 	}
 
 	public void run() {
 		try {
 			Thread.sleep(timeout * 1000);
-			System.out.println("Interrupted");
-			this.watchingThread.interrupt();
+			driver.close();
 		} catch (InterruptedException ex) {
 			return;
 		}

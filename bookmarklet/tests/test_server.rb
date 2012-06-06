d1922a9172387354086b92461bc0e509c1002214
Simon Kornblith <simon@simonster.com>
2012-06-05 23:47:02 -0400

    Fix ruby 1.9 issues?

--- bookmarklet/tests/test_server.rb
+++ bookmarklet/tests/test_server.rb
@@ -10,5 +10,5 @@ else
 	config_file = "config.json"
 end
 $config = JSON.parse(File.read(config_file))
-payload = File.read($config["testPayload"])
+payload = [File.read($config["testPayload"])]
 Rack::Handler::WEBrick.run proc {|env| [200, {"Content-Type" => "application/javascript"}, payload]}, :Port => 31330

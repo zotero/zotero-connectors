7d9253785f632cc9f5a95c9de089bbd47ec50e8f
Simon Kornblith <simon@simonster.com>
2012-08-01 20:08:40 -0400

    Handle case where translator metadata is missing or invalid

--- src/bookmarklet/translator.js
+++ src/bookmarklet/translator.js
@@ -281,8 +281,11 @@ Zotero.Translators = new function() {
 				"for(var $3_zForEachIndex in $3_zForEachSubject)$5{ "+
 				"$2$3 = $3_zForEachSubject[$3_zForEachIndex];", code);
 			if(Zotero.isIE) {
-				var info = infoRe.exec(code)[0];
-				code = info+explorerify(code.substr(info.length));
+				var info = infoRe.exec(code);
+				if(info) {
+					info = info[0];
+					code = info+explorerify(code.substr(info.length));
+				}
 			}
 		}
 		return code;

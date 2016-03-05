zotero/zotero-connectors

commit 294a17bf3b64594884242a7b5f8dc567cdcab6d4

Bail on build script failure

build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # Copyright (c) 2012  Zotero
 #                     Center for History and New Media
@@ -282,6 +282,7 @@ cp "$CWD/icons/Icon-32.png" "$CWD/icons/Icon-48.png" "$CWD/icons/Icon-64.png" \
 rm -rf "$BUILDDIR/chrome/images"
 mkdir "$BUILDDIR/chrome/images"
 cp $ICONS $IMAGES $PREFS_IMAGES "$BUILDDIR/chrome/images"
+# Use 2x images, since Chrome actually wants 19px icons
 for img in "$BUILDDIR"/chrome/images/*2x.png; do
 	mv $img `echo $img | sed 's/@2x//'`
 done

commit a65c5306490ec990a4dad5474abc8398571ec498

Allow build if ImageMagick isn't installed
Broken by 294a17bf3b

build.sh
@@ -260,8 +260,11 @@ echo -n "Building connectors..."
 rm -rf "$BUILDDIR/safari.safariextension/images"
 mkdir "$BUILDDIR/safari.safariextension/images"
 mkdir "$BUILDDIR/safari.safariextension/images/toolbar"
+set +e
 convert -version > /dev/null 2>&1
-if [ $? == 0 ]; then
+RETVAL=$?
+set -e
+if [ $RETVAL == 0 ]; then
 	cp $ICONS "$BUILDDIR/safari.safariextension/images"
 	cp $IMAGES $PREFS_IMAGES "$BUILDDIR/safari.safariextension/images"
 	for f in $ICONS
@@ -270,6 +273,7 @@ if [ $? == 0 ]; then
 				Opacity -contrast-stretch 50 "$BUILDDIR/safari.safariextension/images/toolbar/"`basename $f`
 	done
 else
+	echo
 	echo "ImageMagick not installed; not creating monochrome Safari icons"
 	cp $ICONS "$BUILDDIR/safari.safariextension/images"
 	cp $ICONS "$BUILDDIR/safari.safariextension/images/toolbar"
zotero/zotero-connectors

commit 79d00e90a95fa2571cf63eec1a8283bdb5a4cb80

Closes #74. Jumps back to previous when connector created tab is closed

src/browserExt/background.js
@@ -203,8 +203,14 @@ Zotero.Connector_Browser = new function() {
 		return Zotero.Promise.all(promises);
 	};
 
-	this.openTab = function(url) {
-		chrome.tabs.create({url});
+	this.openTab = function(url, tab) {
+		var tabProps = {};
+		if (tab) {
+			tabProps = {openerTabId: tab.id};
+			chrome.tabs.create(Object.assign({url}, tabProps));
+		} else {
+			chrome.tabs.query({active: true, lastFocusedWindow: true}, (tabs) => this.openTab(url, tabs[0]));
+		}
 	};
 
 	/**

commit 17989273708e1dd63e215445a7d9165f3295bf9e

Addresses #76. Restores firefox tab opening behaviour.
Regression from 79d00e90a.

src/browserExt/background.js
@@ -194,7 +194,12 @@ Zotero.Connector_Browser = new function() {
 	this.openTab = function(url, tab) {
 		var tabProps = {};
 		if (tab) {
-			tabProps = {openerTabId: tab.id};
+			// Firefox does not support openerTabId, but we want to jump to previous tab on close
+			if (Zotero.isFirefox) {
+				tabProps = {index: tab.index+1};
+			} else {
+				tabProps = {openerTabId: tab.id};
+			}
 			chrome.tabs.create(Object.assign({url}, tabProps));
 		} else {
 			chrome.tabs.query({active: true, lastFocusedWindow: true}, (tabs) => this.openTab(url, tabs[0]));
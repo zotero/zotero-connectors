zotero/zotero-connectors

commit 01f7a1815d9c9586a8608e0873bd7d96143835be

First-use UI changes
- Show Z only in Firefox
- Keep showing it until clicked, even if client is open, since upgraders
  need to know what's going on, even if they use Standalone. We can
  remove this altogether once most people have upgraded.
- On click, instead of opening a tab, show a modal alert

src/browserExt/background.js
@@ -94,7 +94,6 @@ Zotero.Connector_Browser = new function() {
 	 */
 	this.onStateChange = function(isOnline) {
 		if (isOnline) {
-			Zotero.Prefs.set('firstUseNoClient', false);
 			Zotero.ContentTypeHandler.enable();
 		} else {
 			for (var i in _tabInfo) {
@@ -208,7 +207,7 @@ Zotero.Connector_Browser = new function() {
 	 * Update status and tooltip of Zotero button
 	 */
 	function _updateExtensionUI(tab) {
-		if (Zotero.Prefs.get('firstUseNoClient')) return _showFirstTimeUI(tab);
+		if (Zotero.Prefs.get('firstUse')) return _showFirstUseUI(tab);
 		chrome.contextMenus.removeAll();
 
 		if (_isDisabledForURL(tab.url)) {
@@ -240,14 +239,10 @@ Zotero.Connector_Browser = new function() {
 		}
 	}
 	
-	function _showFirstTimeUI(tab) {
-		if (Zotero.isFirefox) {
-			let platform = Zotero.platform;
-			var icon = `${Zotero.platform}/zotero-z-${window.devicePixelRatio > 1 ? 32 : 16}px-australis.png`;
-		}
-		else {
-			var icon = 'zotero-z-16px-offline.png';
-		}
+	function _showFirstUseUI(tab) {
+		if (!Zotero.isFirefox) return;
+		
+		var icon = `${Zotero.platform}/zotero-z-${window.devicePixelRatio > 1 ? 32 : 16}px-australis.png`;
 		chrome.browserAction.setIcon({
 			tabId: tab.id,
 			path: `images/${icon}`
@@ -390,9 +385,10 @@ Zotero.Connector_Browser = new function() {
 	}
 	
 	function _browserAction(tab) {
-		if (Zotero.Prefs.get('firstUseNoClient')) {
-			Zotero.Prefs.set('firstUseNoClient', false);
-			Zotero.Connector_Browser.openTab('https://www.zotero.org/connector_start');
+		if (Zotero.Prefs.get('firstUse') && Zotero.isFirefox) {
+			chrome.tabs.sendMessage(tab.id, ["firstUse"], null);
+			Zotero.Prefs.set('firstUse', false);
+			_updateExtensionUI(tab);
 		}
 		else if(_tabInfo[tab.id] && _tabInfo[tab.id].translators && _tabInfo[tab.id].translators.length) {
 			_saveWithTranslator(tab, 0);

src/common/inject/inject.jsx
@@ -169,6 +169,19 @@ Zotero.Inject = new function() {
 		return deferred.promise;	
 	};
 	
+	this.firstUsePrompt = function () {
+		return this.confirm({
+			title: "You’ve installed the Zotero Connector!",
+			button1Text: "Got it",
+			button2Text: "",
+			message: `
+				The Zotero Connector enables you to save references to Zotero from your web browser in a single click.<br><br>
+				<em><strong>Looking for your Zotero data?</strong> If you were previously using Zotero for Firefox, you’ll need to <a href="https://www.zotero.org/download">download</a> the standalone Zotero application to access your local Zotero data going forward. For more information, see [blog post forthcoming before 5.0 Final].</em>
+			`,
+			clickOutsideToClose: true
+		});
+	};
+	
 	this.firstSaveToServerPrompt = function() {
 		return this.confirm({
 			checkbox: true,
@@ -373,7 +386,10 @@ if(!isHiddenIFrame && (window.location.protocol === "http:" || window.location.p
 		Zotero.Messaging.addMessageListener("pageModified", function() {
 			Zotero.Inject.detect(true);
 		});
-
+		Zotero.Messaging.addMessageListener("firstUse", function() {
+			Zotero.Inject.firstUsePrompt();
+		});
+		
 		// initialize
 		Zotero.initInject();
 		

src/common/zotero.js
@@ -90,9 +90,6 @@ var Zotero = new function() {
 		}
 		
 		Zotero.Prefs.set('previousConnectorVersion', Zotero.version);
-		if (Zotero.Prefs.get('firstUseNoClient') === null) {
-			Zotero.Prefs.set('firstUseNoClient', Zotero.Prefs.get('connector.repo.lastCheck.localTime') == 0)
-		}
 		
 		Zotero.Debug.init();
 		Zotero.Messaging.init();
@@ -223,7 +220,7 @@ Zotero.Prefs = new function() {
 		"interceptKnownFileTypes": true,
 		"allowedInterceptHosts": [],
 		"previousConnectorVersion": "0",
-		"firstUseNoClient": null,
+		"firstUse": true,
 		"firstSaveToServer": true,
 		
 		"proxies.transparent": true,

src/safari/global.html
@@ -85,11 +85,7 @@ Zotero.Connector_Browser = new function() {
 	this.onPerformCommand = function(event) {
 		var command = event.command;
 		var tab = safari.application.activeBrowserWindow.activeTab;
-		if (Zotero.Prefs.get('firstUseNoClient')) {
-			Zotero.Prefs.set('firstUseNoClient', false);
-			Zotero.Connector_Browser.openTab('https://www.zotero.org/connector_start');
-		}
-		else if (command === "zotero-button") {
+		if (command === "zotero-button") {
 			if(tab.translators && tab.translators.length) {
 				Zotero.Connector_Browser.saveWithTranslator(0);
 			} else {
@@ -126,8 +122,6 @@ Zotero.Connector_Browser = new function() {
 				}
 			}
 			_updateButtonStatus();
-		} else {
-			Zotero.Prefs.set('firstUseNoClient', false);
 		}
 	};
 	
@@ -173,7 +167,6 @@ Zotero.Connector_Browser = new function() {
 	 */
 	function _updateButtonStatus() {
 		if (!_zoteroButton) return;
-		if (Zotero.Prefs.get('firstUseNoClient')) return _showFirstTimeUI();
 		
 		var tab = safari.application.activeBrowserWindow.activeTab;
 		if (_isDisabledForURL(tab.url)) {
@@ -191,12 +184,6 @@ Zotero.Connector_Browser = new function() {
 		} else {
 			_showWebpageIcon();
 		}
-	}
-	
-	function _showFirstTimeUI(tab) {
-		_zoteroButton.image = safari.extension.baseURI+"images/toolbar/zotero-new-z-16px.png";
-		_zoteroButton.toolTip = "Zotero Connector";
-		_zoteroButton.disabled = false;
 	}
 	
 	function _showZoteroStatus() {

commit e23995fa9db1eecad23749a9728a072b95a1213c

Fix for translator icon not appearing in Chrome (regression from 01f7a181)

src/browserExt/background.js
@@ -208,7 +208,7 @@ Zotero.Connector_Browser = new function() {
 	 * Update status and tooltip of Zotero button
 	 */
 	function _updateExtensionUI(tab) {
-		if (Zotero.Prefs.get('firstUse')) return _showFirstUseUI(tab);
+		if (Zotero.Prefs.get('firstUse') && Zotero.isFirefox) return _showFirstUseUI(tab);
 		chrome.contextMenus.removeAll();
 
 		if (_isDisabledForURL(tab.url)) {
@@ -241,8 +241,6 @@ Zotero.Connector_Browser = new function() {
 	}
 	
 	function _showFirstUseUI(tab) {
-		if (!Zotero.isFirefox) return;
-		
 		var icon = `${Zotero.platform}/zotero-z-${window.devicePixelRatio > 1 ? 32 : 16}px-australis.png`;
 		chrome.browserAction.setIcon({
 			tabId: tab.id,
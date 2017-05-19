zotero/zotero-connectors

commit b6245974e37de70e85bf344a390e83056b587de8

Prevent busy-waiting loops for notifications

src/browserExt/background.js
@@ -258,15 +258,19 @@ Zotero.Connector_Browser = new function() {
 			if (response != undefined || timedOut) return response;
 			
 			// Tab url changed or tab got removed, hence the undefined response
-			return new Zotero.Promise(function(resolve) {
-				chrome.tabs.get(tab.id, function(tab) {
-					if (!tab) return;
-					// If it still exists try again
-					// But make sure translation scripts are injected first
-					return this.injectTranslationScripts(tab)
-						.then(() => resolve(this.notify(text, buttons, seenTimeout, tab)));
-				}.bind(this));
-			}.bind(this))
+			// Wait half a sec to not run a busy-waiting loop
+			return Zotero.Promise.delay(500)
+			.then(function() {
+				return new Zotero.Promise(function(resolve) {
+					chrome.tabs.get(tab.id, function(tab) {
+						if (!tab) return;
+						// If it still exists try again
+						// But make sure translation scripts are injected first
+						return this.injectTranslationScripts(tab)
+							.then(() => resolve(this.notify(text, buttons, timeout, tab)));
+					}.bind(this));
+				}.bind(this))
+			}.bind(this));
 		}.bind(this));
 	};
 

commit c92d0588df3cd01f483c9b3a04c12c591083a21e

Fix notifications (regression in b6245974e)

src/browserExt/background.js
@@ -269,7 +269,7 @@ Zotero.Connector_Browser = new function() {
 						// If it still exists try again
 						// But make sure translation scripts are injected first
 						return this.injectTranslationScripts(tab)
-							.then(() => resolve(this.notify(text, buttons, timeout, tab)));
+							.then(() => resolve(this.notify(text, buttons, seenTimeout, tab)));
 					}.bind(this));
 				}.bind(this))
 			}.bind(this));
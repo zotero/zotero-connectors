zotero/zotero-connectors

commit 9391f36a1484c32fc20246c42a1532645c8bfabb

Fix some issues with translation in frames
- Frames can display progress window
- Correct order of URLs to Translators.getTranslatorsForLocation()
- Prefer translators from top frame if priority is equal

src/common/messages.js
@@ -76,37 +76,54 @@ var MESSAGES = {
 	Translators: 
 		{
 			get: {
-				preSend: function(translators) {
-					return [Zotero.Translators.serialize(translators, TRANSLATOR_PASSING_PROPERTIES)];
+				background: {
+					preSend: function(translators) {
+						return [Zotero.Translators.serialize(translators, TRANSLATOR_PASSING_PROPERTIES)];
+					}
 				},
-				postReceive: function(translator) {
-					return [new Zotero.Translator(translator)];
+				inject: {
+					postReceive: function(translator) {
+						return [new Zotero.Translator(translator)];
+					}
 				}
 			},
 			getAllForType: {
-				preSend: function(translators) {
-					return [Zotero.Translators.serialize(translators, TRANSLATOR_PASSING_PROPERTIES)];
+				background: {
+					preSend: function(translators) {
+						return [Zotero.Translators.serialize(translators, TRANSLATOR_PASSING_PROPERTIES)];
+					},
 				},
-				postReceive: function(translators) {
-					return [translators.map(function(translator) {return new Zotero.Translator(translator)})];
+				inject: {
+					postReceive: function(translators) {
+						return [translators.map(function(translator) {return new Zotero.Translator(translator)})];
+					}
 				}
 			},
 			getWebTranslatorsForLocation: {
-				preSend: function(data) {
-					return [[Zotero.Translators.serialize(data[0], TRANSLATOR_PASSING_PROPERTIES), data[1]]];
+				background: {
+					preSend: function(data) {
+						return [[Zotero.Translators.serialize(data[0], TRANSLATOR_PASSING_PROPERTIES), data[1]]];
+					}
 				},
-				postReceive: function(data) {
-					// Deserialize to class objects
-					data[0] = data[0].map((translator) => new Zotero.Translator(translator));
-					data[1] = data[1].map((proxy) => proxy && new Zotero.Proxy(proxy));
-					return [[data[0], data[1]]];
+				inject: {
+					postReceive: function(data) {
+						// Deserialize to class objects
+						data[0] = data[0].map((translator) => new Zotero.Translator(translator));
+						data[1] = data[1].map((proxy) => proxy && new Zotero.Proxy(proxy));
+						return [[data[0], data[1]]];
+					}
 				}
 			}
 		},
 	Debug: 
 		{
 			clear: false,
-			log: false,
+			log: {
+				response: false,
+				background: {
+					minArgs: 4
+				}
+			},
 			setStore: false
 		},
 	Connector: 
@@ -140,7 +157,12 @@ var MESSAGES = {
 		},
 	Messaging: 
 		{
-			sendMessage: false
+			sendMessage: {
+				response: false,
+				background: {
+					minArgs: 4
+				}
+			}
 		},
 	API: 
 		{
@@ -169,23 +191,25 @@ var MESSAGES = {
 
 MESSAGES["COHTTP"] = {
 	doGet: {
+		background: {
 			// avoid trying to post responseXML
 			preSend: function(xhr) {
 				return [{responseText: xhr.responseText,
 					status: xhr.status,
 					statusText: xhr.statusText}];
 			},
-			callbackArg: 1
 		},
+	},
 	doPost: {
+		background: {
 			// avoid trying to post responseXML
 			preSend: function(xhr) {
 				return [{responseText: xhr.responseText,
 					status: xhr.status,
 					statusText: xhr.statusText}];
 			},
-			callbackArg: 2
 		}
+	}
 };
 
 if(Zotero.isSafari) {

src/common/messaging.js
@@ -67,10 +67,17 @@ Zotero.Messaging = new function() {
 			var messageParts = messageName.split(MESSAGE_SEPARATOR);
 			var messageConfig = MESSAGES[messageParts[0]][messageParts[1]];
 			
-			if(messageConfig) {
-				var callbackArg = (messageConfig.callbackArg ? messageConfig.callbackArg : args.length-1);
+			if (messageConfig && messageConfig.background) {
+				while (messageConfig.background.minArgs > args.length) {
+					args.push(undefined);
+				}
+			}
+			var shouldRespond = messageConfig && messageConfig.response !== false;
+			if (shouldRespond) {
+				var callbackArg = args.length-1;
 
-				if(args[callbackArg] !== null && args[callbackArg] !== undefined) {
+				// TODO: maybe not needed
+				if (args[callbackArg] !== null && args[callbackArg] !== undefined) {
 					// Just append the callback if it is not there as is the case for promisified functions
 					!messageConfig.callbackArg && args.push(0);
 					callbackArg = (messageConfig.callbackArg ? messageConfig.callbackArg : args.length-1); 
@@ -82,11 +89,14 @@ Zotero.Messaging = new function() {
 						newArgs[i] = arguments[i];
 					}
 					
-					if(messageConfig.preSend) newArgs = messageConfig.preSend.apply(null, newArgs);
+					if (messageConfig.background && messageConfig.background.preSend) {
+						newArgs = messageConfig.background.preSend.apply(null, newArgs);
+					}
 					sendResponseCallback(newArgs);
 				}
 			}
 			args.push(tab);
+			args.push(frameId);
 			
 			var fn = Zotero[messageParts[0]][messageParts[1]];
 			if(!fn) throw new Error("Zotero."+messageParts[0]+"."+messageParts[1]+" is not defined");
@@ -100,7 +110,7 @@ Zotero.Messaging = new function() {
 			Zotero.logError(e);
 		}
 		// Return a value, indicating whether `sendResponseCallback` will be called
-		return !!messageConfig;
+		return shouldRespond;
 	}
 	
 	/**
@@ -118,15 +128,17 @@ Zotero.Messaging = new function() {
 				chrome.tabs.query({active: true, lastFocusedWindow: true},
 					(tabs) => resolve(this.sendMessage(messageName, args, tabs[0], frameId)));
 			}.bind(this));
+			let options = {};
+			if (frameId !== null) options = {frameId};
 			// Firefox returns a promise
 			if (Zotero.isFirefox) {
 				// Firefox throws an error when the receiving end doesn't exist (e.g. before injection)
-				return browser.tabs.sendMessage(tab.id, [messageName, args], {frameId})
+				return browser.tabs.sendMessage(tab.id, [messageName, args], options)
 					.catch(() => undefined);
 			}
 			else {
 				let deferred = Zotero.Promise.defer();
-				chrome.tabs.sendMessage(tab.id, [messageName, args], {frameId}, deferred.resolve);
+				chrome.tabs.sendMessage(tab.id, [messageName, args], options, deferred.resolve);
 				return deferred.promise;
 			}
 		} else if(Zotero.isSafari) {

src/browserExt/background.js
@@ -33,15 +33,25 @@ Zotero.Connector_Browser = new function() {
 	/**
 	 * Called when translators are available for a given page
 	 */
-	this.onTranslators = function(translators, instanceID, contentType, tab) {
+	this.onTranslators = function(translators, instanceID, contentType, tab, frameId) {
 		_enableForTab(tab.id);
-		
-		var oldTranslators = _tabInfo[tab.id] && _tabInfo[tab.id].translators;
-		if (oldTranslators) {
-			if ((oldTranslators.length
-					&& (!translators.length || oldTranslators[0].priority <= translators[0].priority))
-				|| (!oldTranslators.length && !translators.length)) return;
+
+		let existingTranslators = _tabInfo[tab.id] && _tabInfo[tab.id].translators;
+		// If translators already exist for tab we need to figure out if the new translators
+		// are more important/higher priority
+		if (existingTranslators) {
+			if (!translators.length) return;
+			
+			if (existingTranslators.length) {
+				let existingTranslatorsHaveHigherPriority = existingTranslators[0].priority > translators[0].priority;
+				if (existingTranslatorsHaveHigherPriority) return;
+				
+				let priorityEqual = translators[0].priority == existingTranslators[0].priority;
+				let newTranslatorsAreFromTopFrame = frameId == 0;
+				if (priorityEqual && !newTranslatorsAreFromTopFrame) return;
+			}	
 		}
+		
 		var isPDF = contentType == 'application/pdf';
 		_tabInfo[tab.id] = {translators, instanceID, isPDF};
 		
@@ -135,15 +145,14 @@ Zotero.Connector_Browser = new function() {
 	 * @param url - url of the frame
 	 */
 	this.onFrameLoaded = function(tab, frameId, url) {
-		if (_isDisabledForURL(tab.url)) {
-			_clearInfoForTab(tab.id);
+		if (_isDisabledForURL(tab.url) || _isDisabledForURL(url)) {
 			return;
 		}
 		// Always inject in the top-frame
 		if (frameId == 0) {
 			return Zotero.Connector_Browser.injectTranslationScripts(tab, frameId);
 		}
-		return Zotero.Translators.getWebTranslatorsForLocation(tab.url, url).then(function(translators) {
+		return Zotero.Translators.getWebTranslatorsForLocation(url, tab.url).then(function(translators) {
 			if (translators[0].length == 0) {
 				Zotero.debug("Not injecting. No translators found for [tab.url, url]: " + tab.url + " , " + url);
 				return;
@@ -169,7 +178,7 @@ Zotero.Connector_Browser = new function() {
 		
 		Zotero.Messaging.sendMessage('ping', null, tab, frameId).then(function(response) {
 			if (response) return deferred.resolve();
-			Zotero.debug(`Injecting translation scripts into ${tab.url}`);
+			Zotero.debug(`Injecting translation scripts into ${frameId} ${tab.url}`);
 			return Zotero.Connector_Browser.injectScripts(_injectTranslationScripts, null, tab, frameId)
 			.then(deferred.resolve).catch(deferred.reject);
 		});
@@ -464,10 +473,12 @@ Zotero.Connector_Browser = new function() {
 	}
 	
 	function _saveWithTranslator(tab, i) {
+		// Set frameId to null - send message to all frames
+		// There is code to figure out which frame should translate with instanceID.
 		Zotero.Messaging.sendMessage("translate", [
 			_tabInfo[tab.id].instanceID,
 			_tabInfo[tab.id].translators[i].translatorID
-		], tab);
+		], tab, null);
 	}
 	
 	function _saveAsWebpage(tab, withSnapshot) {
@@ -523,7 +534,7 @@ Zotero.Connector_Browser = new function() {
 	
 	chrome.webNavigation.onDOMContentLoaded.addListener(function(details) {
 		chrome.tabs.get(details.tabId, Zotero.Utilities.logCallbackError(function(tab) {
-			Zotero.debug("Connector_Browser: onDOMContentLoaded for " + tab.url);
+			Zotero.debug("Connector_Browser: onDOMContentLoaded for " + tab.url + "; " + details.url);
 			Zotero.Connector_Browser.onFrameLoaded(tab, details.frameId, details.url);
 		}));
 	});

src/browserExt/messaging_inject.js
@@ -73,6 +73,9 @@ Zotero.Messaging = new function() {
 						return new Zotero.Promise(function(resolve, reject) {
 							chrome.runtime.sendMessage([messageName, newArgs], function(response) {
 								try {
+									if (messageConfig.inject && messageConfig.inject.postReceive) {
+										response = messageConfig.inject.postReceive.apply(null, response);
+									}
 									if(messageConfig.postReceive) {
 										response = messageConfig.postReceive.apply(null, response);
 									}

src/common/http.js
@@ -42,7 +42,7 @@ Zotero.HTTP = new function() {
 			if(Zotero.isBookmarklet) {
 				Zotero.debug("Attempting cross-site request from bookmarklet; this may fail");
 			} else if(Zotero.isSafari || Zotero.HTTP.isLessSecure(url)) {
-				Zotero.COHTTP.doGet(url, onDone, responseCharset);
+				Zotero.COHTTP.doGet(url, null, responseCharset).then(onDone);
 				return;
 			}
 		}
@@ -97,7 +97,7 @@ Zotero.HTTP = new function() {
 			if(Zotero.isBookmarklet) {
 				Zotero.debug("Attempting cross-site request from bookmarklet; this may fail");
 			} else if(Zotero.isSafari || Zotero.HTTP.isLessSecure(url)) {
-				Zotero.COHTTP.doPost(url, body, onDone, headers, responseCharset);
+				Zotero.COHTTP.doPost(url, body, null, headers, responseCharset).then(onDone);
 				return;
 			}
 		}

src/common/inject/inject.jsx
@@ -32,6 +32,7 @@ if(window.top) {
 		isTopWindow = window.top == window;
 	} catch(e) {};
 }
+var instanceID = isTopWindow ? 0 : (new Date()).getTime();
 
 if(isTopWindow) {
 	/*
@@ -98,7 +99,6 @@ if(isTopWindow) {
 /**
  * @namespace
  */
-var instanceID = (new Date()).getTime();
 Zotero.Inject = new function() {
 	var _translate;
 	this.translators = {};

src/safari/global.js
@@ -46,14 +46,23 @@ Zotero.Connector_Browser = new function() {
 	this.onTranslators = function(translators, instanceID, contentType, tab) {
 		var oldTranslators = tab.translators;
 		tab.contentType = contentType;
-		if (oldTranslators) {
-			if ((oldTranslators.length
-					&& (!translators.length || oldTranslators[0].priority <= translators[0].priority))
-				|| (!oldTranslators.length && !translators.length)) {
-					_updateButtonStatus();
-					return;
-			}
+		
+		let existingTranslators = tab.translators;
+		// If translators already exist for tab we need to figure out if the new translators
+		// are more important/higher priority
+		if (existingTranslators) {
+			if (!translators.length) return;
+			
+			if (existingTranslators.length) {
+				let existingTranslatorsHaveHigherPriority = existingTranslators[0].priority > translators[0].priority;
+				if (existingTranslatorsHaveHigherPriority) return;
+				
+				let priorityEqual = translators[0].priority == existingTranslators[0].priority;
+				let newTranslatorsAreFromTopFrame = instanceID == 0;
+				if (priorityEqual && !newTranslatorsAreFromTopFrame) return;
+			}	
 		}
+		
 		tab.translators = translators;
 		tab.instanceID = instanceID;
 

src/safari/messaging_inject.js
@@ -76,8 +76,8 @@ Zotero.Messaging = new function() {
 								var requestID = Math.floor(Math.random() * 1e12);
 								_callbacks[requestID] = function (response) {
 									try {
-										if (messageConfig.postReceive) {
-											response = messageConfig.postReceive.apply(null, response);
+										if (messageConfig.inject && messageConfig.inject.postReceive) {
+											response = messageConfig.inject.postReceive.apply(null, response);
 										}
 										if (callbackArg !== null) callback.apply(null, response);
 										resolve.apply(null, response);

commit 3746f7a838c44faf686ba6655707525fd7058e9a

Fix a COHTTP regression from 9391f36a1
Closes #134

src/common/messages.js
@@ -191,6 +191,7 @@ var MESSAGES = {
 
 MESSAGES["COHTTP"] = {
 	doGet: {
+		callbackArg: 1,
 		background: {
 			// avoid trying to post responseXML
 			preSend: function(xhr) {
@@ -201,6 +202,7 @@ MESSAGES["COHTTP"] = {
 		},
 	},
 	doPost: {
+		callbackArg: 2,
 		background: {
 			// avoid trying to post responseXML
 			preSend: function(xhr) {

src/common/messaging.js
@@ -74,7 +74,7 @@ Zotero.Messaging = new function() {
 			}
 			var shouldRespond = messageConfig && messageConfig.response !== false;
 			if (shouldRespond) {
-				var callbackArg = args.length-1;
+				var callbackArg = messageConfig.callbackArg || args.length-1;
 
 				// TODO: maybe not needed
 				if (args[callbackArg] !== null && args[callbackArg] !== undefined) {
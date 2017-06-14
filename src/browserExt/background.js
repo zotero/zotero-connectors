zotero/zotero-connectors

commit 4fdd8f34af549ed2a14b263a73016e930e6aefde

Fixes stalling rejected tabs on firefox
Fixes https://forums.zotero.org/discussion/65608/zotero-5-0-ams-translators-dont-work-at-all
and pottentially other reported random issues with firefox

src/browserExt/background.js
@@ -180,6 +180,9 @@ Zotero.Connector_Browser = new function() {
 		}
 		deferred = Zotero.Promise.defer();
 		this.injectTranslationScripts[key] = deferred;
+		deferred.promise.catch((e) => e).then(function() {
+			delete this.injectTranslationScripts[key];
+		}.bind(this));
 		
 		Zotero.Messaging.sendMessage('ping', null, tab, frameId).then(function(response) {
 			if (response) return deferred.resolve();
@@ -187,10 +190,7 @@ Zotero.Connector_Browser = new function() {
 			return Zotero.Connector_Browser.injectScripts(_injectTranslationScripts, null, tab, frameId)
 			.then(deferred.resolve).catch(deferred.reject);
 		});
-		return deferred.promise.then(function(response) {
-			delete this.injectTranslationScripts[key];
-			return response;
-		}.bind(this));
+		return deferred.promise;
 	};
 
 	/**

commit 4873c84f40ec317b48459ab913492f842e5ec7f4

Fix a stalling tab regression in 4fdd8f3

src/browserExt/background.js
@@ -184,8 +184,8 @@ Zotero.Connector_Browser = new function() {
 			Zotero.debug('Connector_Browser.injectTranslationScripts: Script injection rejected');
 			Zotero.logError(e);
 		}).then(function() {
-			delete this.injectTranslationScripts[key];
-		}.bind(this));
+			delete Zotero.Connector_Browser.injectTranslationScripts[key];
+		});
 		
 		Zotero.Messaging.sendMessage('ping', null, tab, frameId).then(function(response) {
 			if (response) return deferred.resolve();
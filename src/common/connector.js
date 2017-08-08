zotero/zotero-connectors

commit 67dcbded10c613eb554f2a4cb7a60ec5f60b8b15

Report the active URL to the client

src/common/connector.js
@@ -23,6 +23,7 @@
     ***** END LICENSE BLOCK *****
 */
 
+// TODO: refactor this class
 Zotero.Connector = new function() {
 	const CONNECTOR_API_VERSION = 2;
 	
@@ -32,6 +33,7 @@ Zotero.Connector = new function() {
 	// http, so pinging Standalone at http://127.0.0.1 fails.
 	// Disable for all browsers, except IE, which may be used frequently with ZSA
 	this.isOnline = Zotero.isBookmarklet && !Zotero.isIE ? false : null;
+	this.shouldReportActiveURL = true;
 	
 	/**
 	 * Checks if Zotero is online and passes current status to callback
@@ -129,10 +131,26 @@ Zotero.Connector = new function() {
 				fail();
 			}
 		} else {
-			Zotero.Connector.callMethod("ping", {}, function(status) {
-				callback(status !== false);
-			});
+			Zotero.Connector.ping().then(() => callback(true)).catch(() => callback(false));
 		}
+	}
+
+	this.reportActiveURL = function(url) {
+		if (!this.isOnline || !this.shouldReportActiveURL) return;
+		
+		let payload = {data: {activeURL: url}};
+		this.ping(payload);
+	}
+	
+	this.ping = function(payload={}) {
+		var deferred = Zotero.Promise.defer();
+		Zotero.Connector.callMethod("ping", payload, function(response, status) {
+			Zotero.Connector.shouldReportActiveURL = response && response.prefs && response.prefs.shouldReportActiveURL;
+			
+			if (response === false) return deferred.reject(status);
+			return deferred.resolve(response);
+		});
+		return deferred.promise;
 	}
 	
 	/**

src/browserExt/background.js
@@ -544,12 +544,14 @@ Zotero.Connector_Browser = new function() {
 		_clearInfoForTab(tabID);
 		// Rerun translation
 		Zotero.Messaging.sendMessage("pageModified", null, tab);
+		tab.active && Zotero.Connector.reportActiveURL(tab.url);
 	});
 	
 	chrome.tabs.onActivated.addListener(function(activeInfo) {
 		chrome.tabs.get(activeInfo.tabId, Zotero.Utilities.logCallbackError(function(tab) {
 			Zotero.debug("Connector_Browser: onActivated for " + tab.url);
 			Zotero.Connector_Browser.onTabActivated(tab);
+			Zotero.Connector.reportActiveURL(tab.url);
 		}));
 	});
 

src/safari/global.js
@@ -240,6 +240,14 @@ Zotero.Connector_Browser = new function() {
 // register handlers
 safari.application.addEventListener("command", Zotero.Connector_Browser.onPerformCommand, false);
 safari.application.addEventListener("validate", Zotero.Connector_Browser.onValidateCommand, false);
+safari.application.addEventListener('activate', function(e) {
+	Zotero.Connector.reportActiveURL(e.target.url);
+}, true);
+safari.application.addEventListener('navigate', function(e) {
+	if (e.target == safari.application.activeBrowserWindow.activeTab) {
+		Zotero.Connector.reportActiveURL(e.target.url);
+	}
+}, true);
 Zotero.Messaging.addMessageListener("selectDone", Zotero.Connector_Browser.onSelectDone);
 
 // initialize

commit 7cbce64db5e6b756ae20ecd0e917bfc8aaef50fb

Fix offline error with Zotero 4.0
Broken in 67dcbded10, probably

src/common/connector.js
@@ -152,8 +152,7 @@ Zotero.Connector = new function() {
 				Zotero.Connector.shouldReportActiveURL = !!response.prefs.reportActiveURL;
 				Zotero.Connector.automaticSnapshots = !!response.prefs.automaticSnapshots;
 			}
-			
-			return response;
+			return response || {};
 		});
 	}
 	
zotero/zotero-connectors

commit c3793436875830d083794438941105daeb39aa50

If no translators available save a PDF in frame if there is one
Closes #155

src/browserExt/contentTypeHandler.js
@@ -70,42 +70,51 @@ Zotero.ContentTypeHandler = {
 		let URI = url.parse(details.url);
 		let contentType = details.responseHeadersObject['content-type'].split(';')[0];
 		if (Zotero.ContentTypeHandler.cslContentTypes.has(contentType)) {
-			Zotero.ContentTypeHandler.confirm(details, `Add citation style to Zotero?`)
-				.then(function(response) {
-					if (response.button == 1) {
-						Zotero.debug(`ContentTypeHandler: Importing style ${details.url}`);
-						Zotero.ContentTypeHandler.importFile(details, 'csl');
-					}
-				}
-			);
-			// Don't continue until we get confirmation
-			return {redirectUrl: 'javascript:'};
+			return this.handleStyle(details);
 		} else if (Zotero.Prefs.get('interceptKnownFileTypes') && 
-			Zotero.ContentTypeHandler.importContentTypes.has(contentType)) {
-			
-			let hosts = Zotero.Prefs.get('allowedInterceptHosts');
-			let isEnabledHost = hosts.indexOf(URI.host) != -1;
-			if (isEnabledHost) {
-				Zotero.debug(`ContentTypeHandler: Importing a file ${details.url}`);
-				Zotero.ContentTypeHandler.importFile(details, 'import');
-			} else {
-				Zotero.ContentTypeHandler.confirm(details, `Import items from ${URI.host} into Zotero?<br/><br/>` +
-					'You can manage automatic file importing in Zotero Connector preferences.',
-					'Always allow for this site').then(function(response) {
-					if (response.button == 1) {
-						Zotero.debug(`ContentTypeHandler: Importing a file ${details.url}`);
-						Zotero.ContentTypeHandler.importFile(details, 'import');
-						if (!isEnabledHost && response.checkboxChecked) {
-							hosts.push(URI.host);
-						}
-						Zotero.Prefs.set('allowedInterceptHosts', hosts);
-					}
-				});
-			}
-			return {redirectUrl: 'javascript:'};
+				Zotero.ContentTypeHandler.importContentTypes.has(contentType)) {
+			return this.handleImportContent(details);
+		} else if (contentType == 'application/pdf') {
+			setTimeout(() => Zotero.Connector_Browser.onPDFFrame(details.url, details.frameId, details.tabId));
 		}
 	},
-
+	
+	handleStyle: function(details) {
+		Zotero.ContentTypeHandler.confirm(details, `Add citation style to Zotero?`)
+			.then(function(response) {
+				if (response.button == 1) {
+					Zotero.debug(`ContentTypeHandler: Importing style ${details.url}`);
+					Zotero.ContentTypeHandler.importFile(details, 'csl');
+				}
+			}
+		);
+		// Don't continue until we get confirmation
+		return {redirectUrl: 'javascript:'};	
+	},
+	
+	handleImportContent: function(details) {
+		let hosts = Zotero.Prefs.get('allowedInterceptHosts');
+		let isEnabledHost = hosts.indexOf(URI.host) != -1;
+		if (isEnabledHost) {
+			Zotero.debug(`ContentTypeHandler: Importing a file ${details.url}`);
+			Zotero.ContentTypeHandler.importFile(details, 'import');
+		} else {
+			Zotero.ContentTypeHandler.confirm(details, `Import items from ${URI.host} into Zotero?<br/><br/>` +
+				'You can manage automatic file importing in Zotero Connector preferences.',
+				'Always allow for this site').then(function(response) {
+				if (response.button == 1) {
+					Zotero.debug(`ContentTypeHandler: Importing a file ${details.url}`);
+					Zotero.ContentTypeHandler.importFile(details, 'import');
+					if (!isEnabledHost && response.checkboxChecked) {
+						hosts.push(URI.host);
+					}
+					Zotero.Prefs.set('allowedInterceptHosts', hosts);
+				}
+			});
+		}
+		return {redirectUrl: 'javascript:'};	
+	},
+	
 	/**
 	 * Handle confirmation prompt by sending a message to injected script and
 	 * redirect to the URL if they click cancel

src/browserExt/background.js
@@ -57,6 +57,22 @@ Zotero.Connector_Browser = new function() {
 		
 		_updateExtensionUI(tab);
 	}
+
+	/**
+	 * If there's a frame with a PDF mimeType this gets invoked
+	 * @param frameURL
+	 * @param tabId
+	 */
+	this.onPDFFrame = function(frameURL, frameId, tabId) {
+		if (_tabInfo[tabId] && _tabInfo[tabId].translators.length) {
+			return;
+		}
+		chrome.tabs.get(tabId, function(tab) {
+			_tabInfo[tab.id] = {translators: [], isPDF: true, frameId};
+			Zotero.Connector_Browser.injectTranslationScripts(tab, frameId);
+			_updateExtensionUI(tab);
+		});
+	}
 	
 	/**
 	 * Called to display select items dialog
@@ -315,7 +331,7 @@ Zotero.Connector_Browser = new function() {
 			_showTranslatorIcon(tab, translators[0]);
 			_showTranslatorContextMenuItem(translators);
 		} else if (isPDF) {
-			_showPDFIcon(tab);
+			Zotero.Connector_Browser._showPDFIcon(tab);
 		} else {
 			_showWebpageIcon(tab);
 		}
@@ -410,7 +426,7 @@ Zotero.Connector_Browser = new function() {
 		chrome.browserAction.setTitle({tabId: tab.id, title});
 	}
 	
-	function _showPDFIcon(tab) {
+	this._showPDFIcon = function(tab) {
 		chrome.browserAction.setIcon({
 			tabId: tab.id,
 			path: chrome.extension.getURL('images/pdf.png')
@@ -501,9 +517,13 @@ Zotero.Connector_Browser = new function() {
 		else if(_tabInfo[tab.id] && _tabInfo[tab.id].translators && _tabInfo[tab.id].translators.length) {
 			Zotero.Connector_Browser._saveWithTranslator(tab, 0);
 		} else {
-			let withSnapshot = Zotero.Connector.isOnline ? Zotero.Connector.automaticSnapshots :
-				Zotero.Prefs.get('automaticSnapshots');
-			Zotero.Connector_Browser._saveAsWebpage(tab, withSnapshot);
+			if (_tabInfo[tab.id] && _tabInfo[tab.id].isPDF) {
+				Zotero.Connector_Browser._saveAsWebpage(tab, _tabInfo[tab.id].frameId, true);
+			} else {
+				let withSnapshot = Zotero.Connector.isOnline ? Zotero.Connector.automaticSnapshots :
+					Zotero.Prefs.get('automaticSnapshots');
+				Zotero.Connector_Browser._saveAsWebpage(tab, 0, withSnapshot);
+			}
 		}
 	}
 	
@@ -516,9 +536,9 @@ Zotero.Connector_Browser = new function() {
 		], tab, null);
 	}
 	
-	this._saveAsWebpage = function(tab, withSnapshot) {
+	this._saveAsWebpage = function(tab, frameId, withSnapshot) {
 		if (tab.id != -1) {
-			return Zotero.Messaging.sendMessage("saveAsWebpage", [tab.title, withSnapshot], tab);
+			return Zotero.Messaging.sendMessage("saveAsWebpage", [tab.title, withSnapshot], tab, frameId);
 		}
 		// Handle right-click on PDF overlay, which exists in a weird non-tab state
 		else {

src/common/inject/inject.jsx
@@ -129,15 +129,6 @@ Zotero.Inject = new function() {
 			if(!_translate) {
 				var me = this;
 				_translate = new Zotero.Translate.Web();
-				_translate.setHandler("translators", function(obj, translators) {
-					me.translators = {};
-					for (let translator of translators) {
-						me.translators[translator.translatorID] = translator;
-					}
-					
-					translators = translators.map(function(translator) {return translator.serialize(TRANSLATOR_PASSING_PROPERTIES)});
-					Zotero.Connector_Browser.onTranslators(translators, instanceID, document.contentType);
-				});
 				_translate.setHandler("select", function(obj, items, callback) {
 					Zotero.Connector_Browser.onSelect(items).then(function(returnItems) {
 						// if no items selected, close save dialog immediately
@@ -182,9 +173,27 @@ Zotero.Inject = new function() {
 				}, false);
 			}
 			_translate.setDocument(document);
-			return _translate.getTranslators(true);
+			return _translate.getTranslators(true).then(function(translators) {
+				if (!translators.length && Zotero.isSafari) {
+					return Zotero.Inject.checkPDFFrames();
+				}
+				me.translators = {};
+				for (let translator of translators) {
+					me.translators[translator.translatorID] = translator;
+				}
+				
+				translators = translators.map(function(translator) {return translator.serialize(TRANSLATOR_PASSING_PROPERTIES)});
+				Zotero.Connector_Browser.onTranslators(translators, instanceID, document.contentType);
+			});
 		} catch(e) {
 			Zotero.logError(e);
+		}
+	};
+	
+	this.checkPDFFrames = function() {
+		if (!isTopWindow) {
+			document.contentType == 'application/pdf';
+			return Zotero.Connector_Browser.onPDFFrame(document.location.href, instanceID);
 		}
 	};
 	
@@ -376,8 +385,8 @@ Zotero.Inject = new function() {
 	};
 	
 	this.saveAsWebpage = function (args) {
-		var title = args[0], withSnapshot = args[1];
-		var progress;
+		var title = args[0] || document.title, withSnapshot = args[1];
+		var image;
 		return Zotero.Inject.checkActionToServer().then(function(result) {
 			if (!result) return;
 			
@@ -390,20 +399,19 @@ Zotero.Inject = new function() {
 			
 			if (document.contentType == 'application/pdf') {
 				data.pdf = true;
-				var image = "attachment-pdf";
+				image = "attachment-pdf";
 			} else {
-				var image = "webpage";
+				image = "webpage";
 			}
-			
-			progress = new Zotero.ProgressWindow.ItemProgress(
-				Zotero.ItemTypes.getImageSrc(image), title || document.title
-			);
+
+			Zotero.Messaging.sendMessage("progressWindow.show", null);
+			Zotero.Messaging.sendMessage("progressWindow.itemSaving",
+				[Zotero.ItemTypes.getImageSrc(image), title, title]);
 			return Zotero.Connector.callMethodWithCookies("saveSnapshot", data)
 		}.bind(this)).then(function(result) {
-			if (progress) {
-				progress.setProgress(100);
-				Zotero.ProgressWindow.startCloseTimer(2500);
-			}
+			Zotero.Messaging.sendMessage("progressWindow.itemProgress",
+				[Zotero.ItemTypes.getImageSrc(image), title, title, 100]);
+			Zotero.Messaging.sendMessage("progressWindow.done", [true]);
 			return result;
 		}.bind(this), function(e) {
 			var err;
@@ -411,18 +419,16 @@ Zotero.Inject = new function() {
 			if (e.status === 0) {
 				// Attempt saving to server if not pdf
 				if (document.contentType != 'application/pdf') {
-					Zotero.ProgressWindow.changeHeadline('Saving to zotero.org');
 					let itemSaver = new Zotero.Translate.ItemSaver({});
 					return itemSaver.saveAsWebpage().then(function(items) {
 						if (items.length) progress.setProgress(100);
 					});
 				} else {
-					err = new Zotero.ProgressWindow.ErrorMessage("clientRequired");
+					Zotero.Messaging.sendMessage("progressWindow.done", [false, 'clientRequired']);
 				}
-			} else {
-				err = new Zotero.ProgressWindow.ErrorMessage("unexpectedError");
+			} else if (!e.value || e.value.libraryEditable != false) {
+				Zotero.Messaging.sendMessage("progressWindow.done", [false, 'unexpectedError']);
 			}
-			Zotero.ProgressWindow.startCloseTimer(8000);	
 			if (err) throw err;
 		}.bind(this));
 	};
@@ -457,9 +463,6 @@ if(!isHiddenIFrame && (isWeb || isTestPage)) {
 		// initialize
 		Zotero.initInject();
 		
-		// Send page load event to clear current save icon/data
-		if(isTopWindow) Zotero.Connector_Browser.onPageLoad();
-	
 		if(document.readyState !== "complete") {
 			window.addEventListener("load", function(e) {
 				if(e.target !== document) return;

src/common/messages.js
@@ -205,6 +205,7 @@ MESSAGES.COHTTP = {
 if(Zotero.isSafari) {
 	MESSAGES.API.createItem = true;
 	MESSAGES.API.uploadAttachment = false;
+	MESSAGES.Connector_Browser.onPDFFrame = false;
 	// Override, because tests don't work in Safari and this causes errors in normal function
 	MESSAGES.Messaging.sendMessage = false;
 }

src/common/test/data/framePDF.html
@@ -26,29 +26,9 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Connector Tests</title>
-	<link href="/lib/mocha.css" rel="stylesheet" type="text/css">
+	<title>Frame PDF</title>
 </head>
 <body>
-<div id="mocha"></div>
-<div id="messages"></div>
-<div id="fixtures"></div>
-<script src="../../zotero_config.js"></script>
-<script src="../../zotero.js"></script>
-<script src="../../promise.js"></script>
-<script src="../../messages.js"></script>
-<script src="../../messaging_inject.js"></script>
-<script src="../../lib/bluebird.js"></script>
-<script src="../../lib/mocha.js"></script>
-<script src="../../lib/chai.js"></script>
-<script src="messages.js"></script>
-<script src="testSetup.js"></script>
-<script src="tests/testSetupTest.js"></script>
-<script src="tests/backgroundTest.js"></script>
-<script src="tests/connectorTest.js"></script>
-<script src="tests/contentTypeHandlerTest.js"></script>
-<script src="tests/preferencesTest.js"></script>
-<script src="tests/translationTest.js"></script>
-<script src="tests/httpTest.js"></script>
+<iframe src="http://zotero-static.s3.amazonaws.com/test.pdf"></iframe>
 </body>
 </html>

src/common/test/testSetup.js
@@ -126,6 +126,11 @@ setTimeout(function() {
 
 if (typeof mocha != 'undefined') {
 	// test.html
+	/**
+	 * @param code {Function|String}
+	 * @params {Object} ... parameters to be passed into the function to be run
+	 * @returns {Promise} return value of the function
+	 */
 	var background = Promise.coroutine(function* (code) {
 		if (typeof code == 'function') {
 			arguments[0] = code.toString();

src/common/test/tests/backgroundTest.js
@@ -1,0 +1,52 @@
+/*
+	***** BEGIN LICENSE BLOCK *****
+	
+	Copyright © 2017 Center for History and New Media
+					George Mason University, Fairfax, Virginia, USA
+					http://zotero.org
+	
+	This file is part of Zotero.
+	
+	Zotero is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	Zotero is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with Zotero.  If not, see <http://www.gnu.org/licenses/>.
+	
+	***** END LICENSE BLOCK *****
+*/
+
+describe('Connector_Browser', function() {
+	var tab = new Tab();
+	
+	describe('onPDFFrame', function() {
+		it('sets icon to PDF if no translators present', Promise.coroutine(function* () {
+			try {
+				let bgPromise = background(function() {
+					Zotero.Prefs.set('firstUse', false);
+					let stub = sinon.stub(Zotero.Connector_Browser, '_showPDFIcon');
+					var deferred = Zotero.Promise.defer();
+					stub.callsFake(deferred.resolve);
+					return deferred.promise;
+				});
+				yield tab.init(chrome.extension.getURL('test/data/framePDF.html'));
+				yield bgPromise;
+				let tabId = yield background(function() {
+					let tabId = Zotero.Connector_Browser._showPDFIcon.args[0][0].id;
+					return tabId;
+				});
+				assert.equal(tabId, tab.tabId);
+			} finally {
+				yield tab.close();
+				yield background(() => Zotero.Connector_Browser._showPDFIcon.restore())
+			}
+		}));
+	});
+});

src/common/test/tests/contentTypeHandlerTest.js
@@ -1,0 +1,50 @@
+/*
+	***** BEGIN LICENSE BLOCK *****
+	
+	Copyright © 2017 Center for History and New Media
+					George Mason University, Fairfax, Virginia, USA
+					http://zotero.org
+	
+	This file is part of Zotero.
+	
+	Zotero is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	Zotero is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with Zotero.  If not, see <http://www.gnu.org/licenses/>.
+	
+	***** END LICENSE BLOCK *****
+*/
+
+describe("ContentTypeHandler", function() {
+	describe('#observe()', function() {
+		it('calls Zotero.Connector_Browser.onPDFFrame when pdf frame loads', Promise.coroutine(function* () {
+			let args = yield background(function() {
+				var stub = sinon.stub(Zotero.Connector_Browser, 'onPDFFrame');
+				Zotero.ContentTypeHandler.observe({frameId: 1, tabId: 1, url: 'test', method: "GET",
+					responseHeadersObject: {'content-type': 'application/pdf'}});
+				return new Zotero.Promise(function(resolve, reject) {
+					// onPDFFrame called out of observe event loop to not stall the page load
+					// since it's a blocking call
+					setTimeout(function() {
+						try {
+							resolve(stub.args[0])
+						} catch (e) {
+							reject(e);
+						} finally {
+							stub.restore();
+						}
+					}, 10);
+				});
+			});
+			assert.deepEqual(args, ['test', 1, 1]);
+		}));
+	});
+});

commit 84b1051610ce6cbec769dc2f5b72cd41019c1528

Fix a ContentTypeHandler regression from c37934368

src/browserExt/contentTypeHandler.js
@@ -67,13 +67,12 @@ Zotero.ContentTypeHandler = {
 			return;
 		}
 		
-		let URI = url.parse(details.url);
 		let contentType = details.responseHeadersObject['content-type'].split(';')[0];
 		if (Zotero.ContentTypeHandler.cslContentTypes.has(contentType)) {
-			return this.handleStyle(details);
+			return Zotero.ContentTypeHandler.handleStyle(details);
 		} else if (Zotero.Prefs.get('interceptKnownFileTypes') && 
 				Zotero.ContentTypeHandler.importContentTypes.has(contentType)) {
-			return this.handleImportContent(details);
+			return Zotero.ContentTypeHandler.handleImportContent(details);
 		} else if (contentType == 'application/pdf') {
 			setTimeout(() => Zotero.Connector_Browser.onPDFFrame(details.url, details.frameId, details.tabId));
 		}
@@ -93,6 +92,7 @@ Zotero.ContentTypeHandler = {
 	},
 	
 	handleImportContent: function(details) {
+		let URI = url.parse(details.url);
 		let hosts = Zotero.Prefs.get('allowedInterceptHosts');
 		let isEnabledHost = hosts.indexOf(URI.host) != -1;
 		if (isEnabledHost) {
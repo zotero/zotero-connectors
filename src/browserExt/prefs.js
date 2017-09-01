zotero/zotero-connectors

commit 3452d2da81e56e27c9730d5f750ca37796329ab4

Move browserExt prefs to chrome.storage API. Closes #96

src/browserExt/prefs.js
@@ -1,0 +1,74 @@
+/*
+	***** BEGIN LICENSE BLOCK *****
+	
+	Copyright © 2017 Center for History and New Media
+					George Mason University, Fairfax, Virginia, USA
+					http://zotero.org
+	
+	This file is part of Zotero.
+	
+	Zotero is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	Zotero is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with Zotero.  If not, see <http://www.gnu.org/licenses/>.
+	
+	***** END LICENSE BLOCK *****
+*/
+
+/**
+ * There's a limit of 5MB of locally stored data.
+ * https://developer.chrome.com/extensions/storage#property-local
+ */
+Zotero.Prefs = Object.assign(Zotero.Prefs, {
+	init: Zotero.Promise.method(function() {
+		return this.migrate().then(function() {
+			return new Zotero.Promise(resolve => chrome.storage.local.get(null, resolve));
+		}.bind(this)).then(function(prefs) {
+			this.syncStorage = prefs;
+		}.bind(this));
+	}),
+	
+	migrate: Zotero.Promise.method(function() {
+		if (!localStorage.length) return;
+		let prefs = Object.assign({}, localStorage);
+		for (let k of Object.keys(prefs)) {
+			if (k.substr(0, 'pref-'.length) == 'pref-') {
+				prefs[k.substr('pref-'.length)] = JSON.parse(prefs[k]);
+			}
+			delete prefs[k];
+		}
+		return new Zotero.Promise(resolve => chrome.storage.local.set(prefs, resolve)).then(function() {
+			return new Zotero.Promise(resolve => chrome.storage.local.set(
+				{translatorMetadata: JSON.parse(localStorage['translatorMetadata'])}, resolve));
+		}).then(function() {
+			localStorage.clear();
+		});
+	}),
+	
+	set: function(pref, value) {
+		Zotero.debug("Setting "+pref+" to "+JSON.stringify(value).substr(0, 100));
+		let prefs = {};
+		prefs[pref] = value;
+
+		this.syncStorage[pref] = value;
+		return new Zotero.Promise(function(resolve) {
+			chrome.storage.local.set(prefs, resolve);
+		});
+	},
+
+	clear: function(pref) {
+		if (Array.isArray(pref)) return Zotero.Promise.all(pref.map((p) => this.clear(p)));
+		delete this.syncStorage[pref];
+		return new Zotero.Promise(function(resolve) {
+			chrome.storage.local.remove(pref, resolve);
+		});
+	}
+});

src/common/translators.js
@@ -44,9 +44,9 @@ Zotero.Translators = new function() {
 	this.init = function(translators) {
 		if(!translators) {
 			translators = [];
-			if((Zotero.isBrowserExt || Zotero.isSafari) && localStorage["translatorMetadata"]) {
+			if((Zotero.isBrowserExt || Zotero.isSafari) && Zotero.Prefs.get("translatorMetadata")) {
 				try {
-					translators = JSON.parse(localStorage["translatorMetadata"]);
+					translators = Zotero.Prefs.get("translatorMetadata");
 					if(typeof translators !== "object") {
 						translators = [];
 					}
@@ -290,9 +290,8 @@ Zotero.Translators = new function() {
 		
 		// Store
 		if (Zotero.isBrowserExt || Zotero.isSafari) {
-			var serialized = JSON.stringify(serializedTranslators);
-			localStorage["translatorMetadata"] = serialized;
-			Zotero.debug("Translators: Saved updated translator list ("+serialized.length+" characters)");
+			Zotero.Prefs.set('translatorMetadata', serializedTranslators);
+			Zotero.debug("Translators: Saved updated translator list ("+serializedTranslators.length+" translators)");
 		}
 		
 		// Reinitialize

src/safari/prefs.js
@@ -1,0 +1,70 @@
+/*
+	***** BEGIN LICENSE BLOCK *****
+	
+	Copyright © 2017 Center for History and New Media
+					George Mason University, Fairfax, Virginia, USA
+					http://zotero.org
+	
+	This file is part of Zotero.
+	
+	Zotero is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	Zotero is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with Zotero.  If not, see <http://www.gnu.org/licenses/>.
+	
+	***** END LICENSE BLOCK *****
+*/
+
+/**
+ * Safari uses localStorage
+ */
+Zotero.Prefs = Object.assign(Zotero.Prefs, {
+	init: Zotero.Promise.method(function() {
+		let prefs = Object.assign({}, localStorage);
+		for (let k of Object.keys(prefs)) {
+			if (k.substr(0, 'pref-'.length) == 'pref-') {
+				prefs[k.substr('pref-'.length)] = JSON.parse(prefs[k]);
+			}
+			delete prefs[k];
+		}
+		this.syncStorage = Object.assign({}, prefs);
+		if ('translatorMetadata' in localStorage) {
+			this.set('translatorMetadata', localStorage['translatorMetadata']);
+			delete localStorage['translatorMetadata'];
+		}
+	}),
+
+	getAsync: function(pref) {
+		return new Zotero.Promise(function(resolve, reject) {
+			if (typeof pref === "object") {
+				var prefData = {};
+				for(var i=0; i<pref.length; i++) {
+					prefData[pref[i]] = Zotero.Prefs.getAsync(pref[i]);
+				}
+				resolve(prefData);
+			} else {
+				resolve(localStorage[`pref-${pref}`]);
+			}	
+		});
+	},
+	
+	set: Zotero.Promise.method(function(pref, value) {
+		Zotero.debug("Setting "+pref+" to "+JSON.stringify(value).substr(0, 100));
+		this.syncStorage[pref] = value;
+		localStorage["pref-"+pref] = JSON.stringify(value);
+	}),
+
+	clear: Zotero.Promise.method(function(pref) {
+		if (Array.isArray(pref)) return pref.forEach((p) => this.clear(p));
+		delete this.syncStorage[pref];
+		delete localStorage[`pref-${pref}`];
+	})
+});

gulpfile.js
@@ -97,6 +97,7 @@ var backgroundInclude = [
 	'zotero_config.js',
 	'zotero.js',
 	'promise.js',
+	'prefs.js',
 	'errors_webkit.js',
 	'api.js',
 	'http.js',

src/common/zotero.js
@@ -95,12 +95,13 @@ var Zotero = new function() {
 			// IE and the likes? Who knows
 			this.platform = 'win';
 		}
-		
-		
-		Zotero.Debug.init();
-		Zotero.Messaging.init();
-		Zotero.Connector_Types.init();
-		Zotero.Repo.init();
+
+		return Zotero.Prefs.init().then(function() {
+			Zotero.Debug.init();
+			Zotero.Messaging.init();
+			Zotero.Connector_Types.init();
+			Zotero.Repo.init();
+		});
 	};
 	
 	/**
@@ -214,6 +215,7 @@ Zotero.Prefs = new function() {
 		"firstUse": true,
 		"firstSaveToServer": true,
 		"reportTranslationFailure": true,
+		"translatorMetadata": [],
 		
 		"proxies.transparent": true,
 		"proxies.autoRecognize": true,
@@ -224,29 +226,29 @@ Zotero.Prefs = new function() {
 		"proxies.clientChecked": false,
 	};
 	
-	this._preloaded = {};
+	this.syncStorage = {};
+
+	/**
+	 * Should override per browser and load data into this.syncStorage
+	 */
+	this.init = function() {throw new Error("Prefs initialization not overriden");};
 	
 	this.get = function(pref) {
-		if (Zotero.isInject) {
-			if (!(pref in this._preloaded)) throw new Error(`Prefs.get: ${pref} not preloaded`);
-			return this._preloaded[pref];
-		}
 		try {
-			if("pref-"+pref in localStorage) return JSON.parse(localStorage["pref-"+pref]);
-		} catch(e) {}
-		if (DEFAULTS.hasOwnProperty(pref)) return DEFAULTS[pref];
-		throw new Error("Zotero.Prefs: Invalid preference "+pref);
+			if (!(pref in this.syncStorage)) throw new Error(`Prefs.get: ${pref} not preloaded`);
+			return this.syncStorage[pref];
+		} catch (e) {
+			if (DEFAULTS.hasOwnProperty(pref)) return DEFAULTS[pref];
+			if (Zotero.isBackground) {
+				throw new Error("Zotero.Prefs: Invalid preference "+pref);
+			} else {
+				throw e;
+			}
+		}
 	};
 	
 	this.getAll = function() {
-		let prefs = Object.assign({}, localStorage);
-		for (let k of Object.keys(prefs)) {
-			if (k.substr(0, 'pref-'.length) == 'pref-') {
-				prefs[k.substr('pref-'.length)] = prefs[k];
-			}
-			delete prefs[k];
-		}
-		return Zotero.Promise.resolve(Object.assign({}, DEFAULTS, prefs));
+		return Zotero.Promise.resolve(Object.assign({}, DEFAULTS, this.syncStorage));
 	};
 	
 	this.getAsync = function(pref) {
@@ -268,30 +270,39 @@ Zotero.Prefs = new function() {
 	};
 
 	/**
-	 * Pre-load a namespace of prefs that can then be accessed synchronously
+	 * Pre-load a namespace of prefs that can then be accessed synchronously.
+	 * Only needed in injected code
 	 *
-	 * (Currently relevant on injected pages, but after switch to asynchronous
-	 * chrome.storage will be relevant everywhere)
 	 * @param namespace {String|String[]}
 	 */
 	this.loadNamespace = function(namespaces) {
+		if (Zotero.isBackground) throw new Error('trying to load namespace in background. all prefs are available via the sync API');
 		if (! Array.isArray(namespaces)) namespaces = [namespaces];
 		return this.getAll().then(function(prefs) {
 			let keys = Object.keys(prefs);
 			for (let namespace of namespaces) {
 				keys.filter((key) => key.indexOf(namespace) === 0)
-					.forEach((key) => this._preloaded[key] = JSON.parse(prefs[key]));
+					.forEach((key) => this.syncStorage[key] = prefs[key]);
 			}
 		}.bind(this));
 	};
-	
+
+	/**
+	 * Should override per browser
+	 * @param pref
+	 * @param value
+	 */
 	this.set = function(pref, value) {
 		Zotero.debug("Setting "+pref+" to "+JSON.stringify(value));
-		localStorage["pref-"+pref] = JSON.stringify(value);
+		this.syncStorage[pref] = value;
 	};
 
+	/**
+	 * Should override per browser
+	 * @param pref
+	 */
 	this.clear = function(pref) {
 		if (Array.isArray(pref)) return pref.forEach((p) => this.clear(p));
-		delete localStorage[`pref-${pref}`];
+		delete this.syncStorage[pref];
 	}
 }

commit 1af9f5d94619c9cb5612e955eea4869eb5e7b603

Add a sanity check for missing translators
A lot of detection failure reports recently in connectors
that is fixed by updating translators. Possibly a regression from
3452d2d if translatorMetadata somehow fails to migrate.

src/browserExt/prefs.js
@@ -46,6 +46,10 @@ Zotero.Prefs = Object.assign(Zotero.Prefs, {
 				}
 				delete prefs[k];
 			}	
+			// If translator metadata migration fails then we need the fetching from repo to
+			// fetch the full list
+			delete prefs["connector.repo.lastCheck.repoTime"];
+			delete prefs["connector.repo.lastCheck.localTime"];
 			browser.storage.local.set(prefs).then(resolve, reject);
 		}).then(function() {
 			if ('translatorMetadata' in localStorage) {

src/common/translators.js
@@ -36,6 +36,7 @@ Zotero.Translators = new function() {
 	var _cache, _translators;
 	var _initialized = false;
 	var _fullFrameDetectionWhitelist = ['resolver.ebscohost.com'];
+	var _resetAttempted = false;
 	
 	/**
 	 * Initializes translator cache, loading all relevant translators into memory
@@ -76,6 +77,17 @@ Zotero.Translators = new function() {
 					Zotero.logError("Could not load translator "+JSON.stringify(translators[i]));
 				} catch(e) {}
 			}
+		}
+		
+		// Huge number of translator metadata missing. Attempt to reset.
+		// NOTE: If the number of translators significantly decreases (currently at 450ish)
+		// then this will trigger on every translator init.
+		if (Object.keys(_translators).length < 400 && !_resetAttempted) {
+			_resetAttempted = true;
+			Zotero.logError(new Error(`Only ${Object.keys(_translators).length} translators present in cache. Resetting`));
+			Zotero.Prefs.clear("connector.repo.lastCheck.repoTime");
+			Zotero.Prefs.clear("connector.repo.lastCheck.localTime");
+			return Zotero.Repo.init();
 		}
 		
 		// Sort by priority
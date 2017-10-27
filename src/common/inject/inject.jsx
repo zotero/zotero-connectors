zotero/zotero-connectors

commit 9d593b5ee7b236ee2bd69fb23f720da746615105

Fix RIS/BibTeX interception when link opens in new tab. Closes #188

src/common/inject/inject.jsx
@@ -79,9 +79,17 @@ if(isTopWindow) {
 	});
 	Zotero.Messaging.addMessageListener("progressWindow.close", Zotero.ProgressWindow.close);
 	Zotero.Messaging.addMessageListener("progressWindow.done", function(returnValue) {
-		if (returnValue[0]) {
+		if (Zotero.isBrowserExt
+			&& document.location.href.startsWith(browser.extension.getURL('confirm.html')))
+		{
+			setTimeout(function() {
+				window.close();
+			}, 1000);
+		}
+		else if (returnValue[0]) {
 			Zotero.ProgressWindow.startCloseTimer(2500);
-		} else {
+		}
+		else {
 			new Zotero.ProgressWindow.ErrorMessage(returnValue[1] || "translationError");
 			Zotero.ProgressWindow.startCloseTimer(8000);
 		}
@@ -455,8 +463,8 @@ try {
 
 // don't try to scrape on hidden frames
 let isWeb = window.location.protocol === "http:" || window.location.protocol === "https:";
-let isTestPage = window.location.protocol.includes('-extension:') && window.location.href.includes('/test/');
-if(!isHiddenIFrame && (isWeb || isTestPage)) {
+let isExtensionPage = window.location.href.startsWith(browser.extension.getURL(''));
+if(!isHiddenIFrame && (isWeb || isExtensionPage)) {
 	var doInject = function () {
 		// add listener for translate message from extension
 		Zotero.Messaging.addMessageListener("translate", function(data) {

gulpfile.js
@@ -195,7 +195,7 @@ function processFile() {
 		var addFiles = function(file) {
 			// Amend paths
 			if (type === 'common' || type === 'browserExt') {
-				if (file.path.includes('test/data') && file.path.includes('.html')) {
+				if (file.path.includes('.html')) {
 					file.contents = Buffer.from(replaceScriptsHTML(
 						file.contents.toString(), "<!--SCRIPTS-->", injectIncludeBrowserExt.map(s => `../../${s}`)));
 				}
@@ -289,7 +289,8 @@ gulp.task('watch-chrome', function () {
 gulp.task('process-custom-scripts', function() {
 	let sources = [
 		'./src/browserExt/background.js',
-		'./src/browserExt/manifest.json', 
+		'./src/browserExt/manifest.json',
+		'./src/browserExt/confirm.html',
 		'./src/safari/global.html',
 		'./src/safari/Info.plist',
 		'./src/common/node_modules.js',

src/browserExt/confirm.html
@@ -1,0 +1,37 @@
+<!DOCTYPE html>
+<!--
+    ***** BEGIN LICENSE BLOCK *****
+    
+    Copyright © 2017 Center for History and New Media
+                     George Mason University, Fairfax, Virginia, USA
+                     http://zotero.org
+    
+    This file is part of Zotero.
+    
+    Zotero is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    Zotero is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    
+    You should have received a copy of the GNU Affero General Public License
+    along with Zotero.  If not, see <http://www.gnu.org/licenses/>.
+    
+    ***** END LICENSE BLOCK *****
+-->
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Zotero - Confirm Import</title>
+</head>
+<body>
+<!--SCRIPTS-->
+<script src="lib/react.js"></script>
+<script src="lib/react-dom.js"></script>
+<script src="ui/modal-prompt.js"></script>
+</body>
+</html>

src/browserExt/contentTypeHandler.js
@@ -145,10 +145,24 @@ Zotero.ContentTypeHandler = {
 			}
 		}
 		let response = await Zotero.Messaging.sendMessage('confirm', props, tab);
-		// If captured URL was pasted on about:blank or other browser pages they respond immediately
-		// with undefined which we treat as cancel here
+		// If captured URL was pasted on about:blank or other browser pages the response is immediate
+		// with undefined and means we cannot inject and display the UI, so we have to do some additional work
 		if (!response) {
-			response = {button: 2}
+			var responsePromise = new Zotero.Promise(function(resolve, reject) {
+				browser.tabs.onUpdated.addListener(async function getResponse(tabId, changeInfo) {
+					try {
+						if (tabId == tab.id && changeInfo.status == 'complete') {
+							let response = await Zotero.ContentTypeHandler.confirm(details, message, checkboxText);
+							browser.tabs.onUpdated.removeListener(getResponse);
+							resolve(response);
+						}
+					} catch (e) {
+						reject(e);
+					}
+				});
+			});
+			browser.tabs.update(tab.id, {url: browser.extension.getURL(`confirm.html`)});
+			response = await responsePromise;
 		}
 
 		return response;

src/common/test/tests/contentTypeHandlerTest.js
@@ -109,32 +109,30 @@ describe("ContentTypeHandler", function() {
 			assert.equal(redirectUrl, 'test');
 		});
 		
-		it('navigates to target url if no injection context available', async function () {
-			let url = 'http://zotero-static.s3.amazonaws.com/test.ris';
-			let redirectUrlPromise = background(async function() {
+		it('navigates to confirmation page if no injection context available', async function () {
+			await background(async function(tabId) {
 				try {
-					var customObserver = d => Zotero.ContentTypeHandler.observe(d);
-					Zotero.WebRequestIntercept.addListener('headersReceived', customObserver);
-					// stubbing Zotero.Messaging.sendMessage('confirm', props, tab);
-					var stub1 = sinon.stub(Zotero.Messaging, 'sendMessage');
-					stub1.resolves({button: 2});
+					let confirm = Zotero.ContentTypeHandler.confirm;
+					var stub2 = sinon.stub(Zotero.ContentTypeHandler, 'confirm');
 					var deferred = Zotero.Promise.defer();
-					var stub2 = sinon.stub(Zotero.ContentTypeHandler, '_redirectToOriginal').callsFake(function(tabId, url) {
-						deferred.resolve(url);
+					stub2.callsFake(async function(details) {
+						let tab = await browser.tabs.get(details.tabId);
+						if (tab.url.includes('confirm.html')) deferred.resolve();
+						return confirm.apply(Zotero.ContentTypeHandler, arguments);
 					});
+					
+					Zotero.ContentTypeHandler.observe({frameId: 1, tabId, url: 'http://www.zotero.org/', method: "GET",
+						responseHeadersObject: {'content-type': 'application/x-research-info-systems'}});
 
 					let result = await deferred.promise;
 					return result;
 				} finally {
-					stub1.restore();
 					stub2.restore();
-					Zotero.WebRequestIntercept.removeListener('headersReceived', customObserver)
 				}
-			});
+			}, tab.id);
+			tab = await browser.tabs.get(tab.id);
 			
-			browser.tabs.update(tab.id, {url});
-			let redirectUrl = await redirectUrlPromise;
-			assert.equal(redirectUrl, url);
+			assert.equal(tab.url, getExtensionURL('confirm.html'));
 		});
 	});
 });

commit 2d26560f81002d66d36283479e53f5eac93f2849

Fix safari regression from 9d593b5e

src/common/inject/inject.jsx
@@ -463,7 +463,8 @@ try {
 
 // don't try to scrape on hidden frames
 let isWeb = window.location.protocol === "http:" || window.location.protocol === "https:";
-let isExtensionPage = window.location.href.startsWith(browser.extension.getURL(''));
+let isExtensionPage = Zotero.isBrowserExt && window.location.href.startsWith(browser.extension.getURL(''))
+	|| Zotero.isSafari && window.location.href.startsWith('safari-extension://');
 if(!isHiddenIFrame && (isWeb || isExtensionPage)) {
 	var doInject = function () {
 		// add listener for translate message from extension

src/common/messaging.js
@@ -143,6 +143,7 @@ Zotero.Messaging = new function() {
 						}
 						deferred.resolve(payload);
 					} else if (event.name === 'Connector_Browser.onPageLoad') {
+						// URL changed, so we resolve all of these
 						deferred.resolve();
 					}
 				}

src/common/test/tests/testSetupTest.js
@@ -96,7 +96,9 @@ describe('TestSetup', function() {
 				yield Zotero.Promise.delay(100);
 				let closedTab;
 				try {
-					closedTab = yield browser.tabs.get(tabId);
+					closedTab = yield background(async function(id) {
+						return !!(await Zotero.Background.getTabByID(id))
+					}, tabId);
 				} catch (e) {return}
 				assert.isNotOk(closedTab, 'Tab was not closed')
 			}));

src/common/test/tests/translationTest.js
@@ -121,22 +121,22 @@ describe("Translation", function() {
 				}));
 			
 				it('saves as snapshot', async function () {
-					await background(async function(tabId) {
-						var stub = sinon.stub(Zotero.Connector, "callMethodWithCookies").resolves([]);
-						let tab = await Zotero.Background.getTabByID(tabId);
-						try {
+					try {
+						await background(async function (tabId) {
+							sinon.stub(Zotero.Connector, "callMethod").resolves([]);
+							let tab = await Zotero.Background.getTabByID(tabId);
 							await Zotero.Connector_Browser.saveAsWebpage(tab, false);
-						} finally {
-							stub.restore()
-						}
-					}, tab.tabId);
-					await Zotero.Promise.delay(20);
-					var message = await tab.run(function() {
-						var message = document.getElementById('zotero-progress-window').textContent;
-						Zotero.ProgressWindow.close();
-						return message;
-					});
-					assert.include(message, "Scarcity or Abundance? Preserving the Past in a Digital Era");	
+						}, tab.tabId);
+						await Zotero.Promise.delay(20);
+						var message = await tab.run(function () {
+							var message = document.getElementById('zotero-progress-window').textContent;
+							Zotero.ProgressWindow.close();
+							return message;
+						});
+						assert.include(message, "Scarcity or Abundance? Preserving the Past in a Digital Era");
+					} finally {
+						await background(() => Zotero.Connector.callMethod.restore())
+					}
 				});
 					
 				it('displays an error message if Zotero responds with an error', async function () {
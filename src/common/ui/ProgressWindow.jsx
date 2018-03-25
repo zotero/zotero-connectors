ee3e3a465ac95221c0edbe0acafac09b8ba24e9d
Dan Stillman <dstillman@zotero.org>
2018-03-25 05:17:06 -0400

    Fix comment in target selector

--- src/common/ui/ProgressWindow.jsx
+++ src/common/ui/ProgressWindow.jsx
@@ -625,9 +625,9 @@ class TargetTree extends React.Component {
 					
 					return (
 						<div className={className} style={{marginLeft: depth * 5 + "px"}}>
-							// Add toggle on arrow click, since we disabled it in tree.js for
-							// clicking on the row itself. If the tree is updated to have less
-							// annoying behavior, this can be reverted.
+							{/* Add toggle on arrow click, since we disabled it in tree.js for
+							    clicking on the row itself. If the tree is updated to have less
+							    annoying behavior, this can be reverted. */}
 							<span onClick={() => this.onRowToggle(item)}>{arrow}</span>
 							<TargetIcon type={type} />
 							<span className="tree-item-label">{item.name}</span>

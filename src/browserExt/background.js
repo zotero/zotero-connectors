zotero/zotero-connectors

commit 9da2caf5dc49c005725538f727bea7f14e27a217

Fix detection for pages that use history for navigation
Translation detection will be reinitialized upon history edit (only
works on BrowserExt). This will fix buginess on APA PsycNET and others

Closes #251

src/browserExt/background.js
@@ -829,24 +829,11 @@ Zotero.Connector_Browser = new function() {
 			}
 		}
 	}
-
-	browser.browserAction.onClicked.addListener(logListenerErrors(_browserAction));
 	
-	browser.tabs.onRemoved.addListener(logListenerErrors(_clearInfoForTab));
-	
-	browser.tabs.onActivated.addListener(logListenerErrors(async function(details) {
-		var tab = await browser.tabs.get(details.tabId);
-		// Ignore item selector
-		if (tab.url.indexOf(browser.extension.getURL("itemSelector/itemSelector.html")) === 0) return;
-		Zotero.debug("Connector_Browser: onActivated for " + tab.url);
-		Zotero.Connector_Browser.onTabActivated(tab);
-		Zotero.Connector.reportActiveURL(tab.url);
-	}));
-	
-	browser.webNavigation.onCommitted.addListener(logListenerErrors(async function(details) {
+	async function onNavigation(details, historyChange=false) {
 		// Ignore developer tools, item selector
 		if (details.tabId < 0 || _isDisabledForURL(details.url, true)
-				|| details.url.indexOf(browser.extension.getURL("itemSelector/itemSelector.html")) === 0) return;
+			|| details.url.indexOf(browser.extension.getURL("itemSelector/itemSelector.html")) === 0) return;
 
 		if (details.frameId == 0) {
 			_updateInfoForTab(details.tabId, details.url);
@@ -864,7 +851,26 @@ Zotero.Connector_Browser = new function() {
 		// executes in the next event loop such that the rejections can be processed
 		await Zotero.Promise.delay(1);
 		await Zotero.Connector_Browser.onFrameLoaded(tab, details.frameId, details.url);
+		if (historyChange) {
+			Zotero.Messaging.sendMessage('historyChanged');
+		}
+	}
+
+	browser.browserAction.onClicked.addListener(logListenerErrors(_browserAction));
+	
+	browser.tabs.onRemoved.addListener(logListenerErrors(_clearInfoForTab));
+	
+	browser.tabs.onActivated.addListener(logListenerErrors(async function(details) {
+		var tab = await browser.tabs.get(details.tabId);
+		// Ignore item selector
+		if (tab.url.indexOf(browser.extension.getURL("itemSelector/itemSelector.html")) === 0) return;
+		Zotero.debug("Connector_Browser: onActivated for " + tab.url);
+		Zotero.Connector_Browser.onTabActivated(tab);
+		Zotero.Connector.reportActiveURL(tab.url);
 	}));
+	
+	browser.webNavigation.onCommitted.addListener(logListenerErrors(onNavigation));
+	browser.webNavigation.onHistoryStateUpdated.addListener(details => logListenerErrors(onNavigation(details, true)));
 }
 
 Zotero.initGlobal();

src/common/inject/inject.jsx
@@ -454,7 +454,14 @@ Zotero.Inject = new function() {
 				} else {
 					// Clear session details on failure, so another save click tries again
 					this.sessionDetails = {};
+					// We delay opening the progressWindow for multiple items so we don't have to flash it
+					// for the select dialog. But it comes back to bite us in the butt if a translation
+					// error occurs immediately since the below command will execute before the progressWindow show,
+					// and then the delayed progressWindow.show will pop up another empty progress window.
+					// Cannot have that!
+					await Zotero.Promise.delay(500);
 					Zotero.Messaging.sendMessage("progressWindow.done", [false]);
+					return;
 				}
 			}
 		}
@@ -595,8 +602,8 @@ const isWeb = window.location.protocol === "http:" || window.location.protocol =
 const isTestPage = Zotero.isBrowserExt && window.location.href.startsWith(browser.extension.getURL('test'));
 // don't try to scrape on hidden frames
 if(!isHiddenIFrame) {
-	var doInject = function () {
-		Zotero.initInject();
+	var doInject = async function () {
+		await Zotero.initInject();
 
 		if (Zotero.isSafari && isTopWindow) {
 			Zotero.Connector_Browser.onPageLoad(document.location.href);
@@ -623,6 +630,10 @@ if(!isHiddenIFrame) {
 		Zotero.Messaging.addMessageListener("pageModified", function() {
 			Zotero.Inject.init(true);
 		});
+		Zotero.Messaging.addMessageListener('historyChanged', function() {
+			Zotero.Inject.init(true);
+		});
+		
 		Zotero.Messaging.addMessageListener("firstUse", function () {
 			return Zotero.Inject.firstUsePrompt();
 		});

commit c1f64a0b79d3a5851f6f18546a21cdc654ad92a4

Fix webpage icon appearing on first load of PDFs in Firefox
Regression from 9da2caf5dc4

src/browserExt/background.js
@@ -837,7 +837,12 @@ Zotero.Connector_Browser = new function() {
 		// Ignore developer tools, item selector
 		if (details.tabId < 0 || _isDisabledForURL(details.url, true)
 			|| details.url.indexOf(browser.extension.getURL("itemSelector/itemSelector.html")) === 0) return;
-
+		
+		// Don't process again if URL hasn't changed
+		if (_tabInfo[details.tabId] && _tabInfo[details.tabId].url == details.url) {
+			return;
+		}
+		
 		if (details.frameId == 0) {
 			_updateInfoForTab(details.tabId, details.url);
 			// Getting the tab is uber slow in Firefox. Since _updateInfoForTab() resets the
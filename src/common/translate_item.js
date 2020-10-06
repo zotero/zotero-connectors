zotero/zotero-connectors

commit 4e0437813aac58436e946d70d5c07a63e654e918

Use SingleFile to create snapshots of webpages

src/common/translate_item.js
@@ -98,13 +98,32 @@ Zotero.Translate.ItemSaver.prototype = {
 		var payload = {
 			items,
 			sessionID: this._sessionID,
-			uri: this._baseURI
+			uri: this._baseURI,
+			singleFile: true
 		};
 		if (Zotero.isSafari) {
 			// This is the best in terms of cookies we can do in Safari
 			payload.cookie = document.cookie;
 		}
 		payload.proxy = this._proxy && this._proxy.toJSON();
+		
+		// Add page data for snapshot
+		let singleFile = false;
+		for (let item of items) {
+			for (let attachment of item.attachments) {
+				if (attachment.mimeType !== 'text/html'
+					|| attachment.snapshot === false) {
+					continue;
+				}
+				if (!this._urlMatchesLocation(attachment.url)) {
+					continue;
+				}
+
+				attachment.singleFile = true;
+				singleFile = true;
+			}
+		}
+		
 		try {
 			var data = await Zotero.Connector.callMethodWithCookies("saveItems", payload)
 		}
@@ -131,9 +150,84 @@ Zotero.Translate.ItemSaver.prototype = {
 			}
 		}
 		this._pollForProgress(data.items, attachmentCallback);
+
+		// If we have a snapshot and the client supports SingleFile snapshots
+		if (singleFile && data && data.singleFile) {
+			// Do not wait for async function so we continue to update UI
+			this._executeSingleFile(payload);
+		}
+
 		return data.items;
 	},
+
+	_executeSingleFile: async function(payload) {
+		try {
+			let { form, pageData } = await Zotero.SingleFile.retrievePageData();
+			payload.pageData = pageData;
+
+			// Form contains all the binary bits, we add the payload as a part
+			// so we can submit the whole multipart structure
+			form.payload = JSON.stringify(payload);
+			payload = form;
+		}
+		catch (e) {
+			// We swallow this error and fall back to saving the
+			// page in the client
+			payload = {
+				payload: JSON.stringify(payload)
+			};
+		}
+
+		await Zotero.Connector.callMethodWithCookies({
+				method: "saveSingleFile",
+				headers: {"Content-Type": "multipart/form-data"}
+			},
+			payload
+		);
+	},
 	
+	/**
+	 * Return true if the attachment URL fuzzy matches the window location
+	 *
+	 * @param {String} attachmentURL
+	 * @return {Boolean}
+	 */
+	_urlMatchesLocation: function(attachmentURL) {
+		// Complete match
+		if (attachmentURL === location.href) {
+			return true;
+		}
+		// Translators control the attachment URL and historically that URL was passed to
+		// the client to save a snapshot. Here we are trying to detect if the attachment URL
+		// has query params that are a subset of the current URL. So for example:
+		// 
+		// Attachment URL: /records?id=1234 would match the following:
+		// 
+		// Current URL: /records?id=1234#abstract
+		// Current URL: /records?id=1234&utm_source=search
+		// Current URL: /records?utm_source=search&id=1234
+		//
+		// But not match:
+		//
+		// Current URL: /records
+		// Current URL: /records?utm_source=search
+		// Current URL: /records?id=5678
+		const url = new URL (attachmentURL);
+		if (url.protocol + url.host + url.pathname === location.protocol + location.host
+				+ location.pathname) {
+			const locationURL = new URL(location.href);
+			for (const [param, value] of url.searchParams) {
+				if (locationURL.searchParams.get(param) !== value) {
+					return false;
+				}
+			}
+
+			return true;
+		}
+
+		return false;
+	},
+
 	_processItems: function(items) {
 		var saveOptions = !Zotero.isBookmarklet && Zotero.Inject.sessionDetails.saveOptions;
 		if (saveOptions && saveOptions.note && items.length == 1) {

build.sh
@@ -284,6 +284,12 @@ function copyResources {
 	
 	# Remove .jsx files - we'll deal with those in gulp
 	find "$browser_builddir" -type f -name "*.jsx" -delete
+
+	# Copy SingleFileZ submodule code
+	mkdir -p "$browser_builddir/lib/SingleFileZ/extension/lib"
+	cp -r "$SRCDIR/zotero/resource/SingleFileZ/extension/lib/single-file" \
+		"$browser_builddir/lib/SingleFileZ/extension/lib/single-file"
+	cp -r "$SRCDIR/zotero/resource/SingleFileZ/lib" "$browser_builddir/lib/SingleFileZ/lib"
 	
 	if [ ! -z $DEBUG ]; then
 		cp "$SRCDIR/zotero/chrome/content/zotero/tools/testTranslators"/*.js \

gulpfile.js
@@ -83,7 +83,8 @@ var injectInclude = [
 	'messaging_inject.js',
 	'inject/progressWindow_inject.js',
 	'inject/modalPrompt_inject.js',
-	'i18n.js'
+	'i18n.js',
+	'singlefile.js'
 ];
 var injectIncludeLast;
 if (argv.p) {
@@ -130,7 +131,14 @@ var backgroundInclude = [
 	'utilities.js',
 	'google-docs-plugin-manager.js',
 	'messages.js',
-	'messaging.js'
+	'messaging.js',
+	'lib/SingleFileZ/lib/single-file/index.js',
+	'lib/SingleFileZ/extension/lib/single-file/index.js',
+	'lib/SingleFileZ/extension/lib/single-file/browser-polyfill/chrome-browser-polyfill.js',
+	'lib/SingleFileZ/extension/lib/single-file/core/bg/scripts.js',
+	'lib/SingleFileZ/extension/lib/single-file/fetch/bg/fetch.js',
+	'lib/SingleFileZ/extension/lib/single-file/frame-tree/bg/frame-tree.js',
+	'lib/SingleFileZ/extension/lib/single-file/lazy/bg/lazy-timeout.js'
 ];
 
 
@@ -187,6 +195,9 @@ function processFile() {
 			}
 		}
 		var type = parts[i];
+
+		// Used to identify files by pathname from src to try and prevent conflicts
+		var sourcefile = parts.slice(i).join('/');
 		
 		// Transform react
 		if (ext == 'jsx') {
@@ -327,6 +338,60 @@ function processFile() {
 				});
 				break;
 		}
+
+		// sourcefile is relative to the src/ directory
+		switch (sourcefile) {
+			case 'zotero/resource/SingleFileZ/extension/lib/single-file/core/bg/scripts.js':
+				// Change base path and add in the content scripts SingleFile recommends injecting
+				// via manifest.json
+				file.contents = Buffer.from(file.contents.toString()
+					.replace('const basePath = "../../../";', 'const basePath = "lib/SingleFileZ/";')
+				);
+
+				// Override the type so we include this file in firefox and chrome builds
+				type = 'browserExt';
+				// Switch from resource to lib sub-directory
+				parts[i+1] = 'lib';
+				break;
+			case 'zotero/resource/SingleFileZ/lib/single-file/processors/hooks/content/content-hooks-frames.js':
+				// Change the path to include our particular directory structure
+				file.contents = Buffer.from(file.contents.toString()
+					.replace('/lib/single-file/processors/hooks/content/content-hooks-frames-web.js',
+					'/lib/SingleFileZ/lib/single-file/processors/hooks/content/content-hooks-frames-web.js')
+				);
+
+				// Override the type so we include this file in firefox and chrome builds
+				type = 'browserExt';
+				// Switch from resource to lib sub-directory
+				parts[i+1] = 'lib';
+				break;
+			case 'zotero/resource/SingleFileZ/lib/single-file/processors/hooks/content/content-hooks.js':
+				// Change the path to include our particular directory structure
+				file.contents = Buffer.from(file.contents.toString()
+					.replace('/lib/single-file/processors/hooks/content/content-hooks-web.js',
+						'/lib/SingleFileZ/lib/single-file/processors/hooks/content/content-hooks-web.js')
+				);
+
+				// Override the type so we include this file in firefox and chrome builds
+				type = 'browserExt';
+				// Switch from resource to lib sub-directory
+				parts[i+1] = 'lib';
+				break;
+			case 'zotero/resource/SingleFileZ/lib/single-file/index.js':
+				// Change the object references to work correctly
+				file.contents = Buffer.from(file.contents.toString()
+					.replace('this.frameTree.content.frames.getAsync',
+						'this.processors.frameTree.content.frames.getAsync')
+					.replace('this.lazy.content.loader.process',
+						'this.processors.lazy.content.loader.process')
+				);
+
+				// Override the type so we include this file in firefox and chrome builds
+				type = 'browserExt';
+				// Switch from resource to lib sub-directory
+				parts[i+1] = 'lib';
+				break;
+		}
 		
 		let f;
 		
@@ -389,7 +454,7 @@ gulp.task('watch', function () {
 			.pipe(processFile())
 			.pipe(gulp.dest((data) => data.base));
 	});
-});
+});  
 
 gulp.task('watch-chrome', function () {
 	var watcher = gulp.watch(['./src/browserExt/**', './src/common/**', './src/safari/**',
@@ -417,7 +482,12 @@ gulp.task('process-custom-scripts', function() {
 		'./src/common/zotero_config.js',
 		'./src/common/test/**/*',
 		'./src/**/*.jsx',
-		'./src/zotero-google-docs-integration/src/connector/**'
+		'./src/zotero-google-docs-integration/src/connector/**',
+		'./src/zotero/resource/SingleFileZ/extension/lib/single-file/core/bg/scripts.js',
+		'./src/zotero/resource/SingleFileZ/lib/single-file/processors/hooks/content/content-hooks.js',
+		'./src/zotero/resource/SingleFileZ/lib/single-file/processors/hooks/content/content-hooks-frames.js',
+		'./src/zotero/resource/SingleFileZ/lib/single-file/index.js',
+		'./src/zotero/resource/SingleFileZ/extension/lib/single-file/index.js'
 	];
 	if (!argv.p) {
 		sources.push('./src/common/test/**/*.js');	

src/browserExt/background.js
@@ -328,6 +328,13 @@ Zotero.Connector_Browser = new function() {
 			clearTimeout(timeout);
 		}
 	};
+
+	this.injectSingleFile = async function(tab, frameId) {
+		Zotero.debug("SingleFile: injecting SingleFile into page");
+		await singlefile.extension.injectScript(tab.id, {});
+		// Also insert the config object
+		await this.injectScripts('singlefile.js', tab, frameId);
+	};
 	
 	this.openWindow = async function(url, options={}, tab=null) {
 		if (!tab) {

src/browserExt/singlefile.js
@@ -1,0 +1,139 @@
+/*
+    ***** BEGIN LICENSE BLOCK *****
+    
+    Copyright © 2020 Corporation for Digital Scholarship
+            Vienna, Virginia, USA
+            https://www.zotero.org
+    
+    This file is part of Zotero.
+    
+    Zotero is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    Zotero is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    
+    You should have received a copy of the GNU Affero General Public License
+    along with Zotero.  If not, see <http://www.gnu.org/licenses/>.
+    
+    ***** END LICENSE BLOCK *****
+*/
+
+Zotero.SingleFile = new function () {
+	// These are defaults from SingleFileZ
+	// Located in: zotero/resources/SingleFileZ/extension/core/bg/config.js
+	this.CONFIG = {
+		removeHiddenElements: true,
+		removeUnusedStyles: true,
+		removeUnusedFonts: true,
+		removeFrames: true,
+		removeImports: true,
+		removeScripts: true,
+		compressHTML: true,
+		compressCSS: false,
+		loadDeferredImages: true,
+		loadDeferredImagesMaxIdleTime: 1500,
+		loadDeferredImagesBlockCookies: false,
+		loadDeferredImagesBlockStorage: false,
+		loadDeferredImagesKeepZoomLevel: false,
+		filenameTemplate: "{page-title} ({date-iso} {time-locale}).html",
+		infobarTemplate: "",
+		includeInfobar: false,
+		confirmInfobarContent: false,
+		autoClose: false,
+		confirmFilename: false,
+		filenameConflictAction: "uniquify",
+		filenameMaxLength: 192,
+		filenameReplacedCharacters: ["~", "+", "\\\\", "?", "%", "*", ":", "|", "\"", "<", ">", "\x00-\x1f", "\x7F"],
+		filenameReplacementCharacter: "_",
+		contextMenuEnabled: true,
+		tabMenuEnabled: true,
+		browserActionMenuEnabled: true,
+		shadowEnabled: true,
+		logsEnabled: true,
+		progressBarEnabled: true,
+		maxResourceSizeEnabled: false,
+		maxResourceSize: 10,
+		removeAudioSrc: true,
+		removeVideoSrc: true,
+		displayInfobar: true,
+		displayStats: false,
+		backgroundSave: true,
+		autoSaveDelay: 1,
+		autoSaveLoad: false,
+		autoSaveUnload: false,
+		autoSaveLoadOrUnload: true,
+		autoSaveRepeat: false,
+		autoSaveRepeatDelay: 10,
+		removeAlternativeFonts: true,
+		removeAlternativeMedias: true,
+		removeAlternativeImages: true,
+		saveRawPage: false,
+		saveToGDrive: false,
+		forceWebAuthFlow: false,
+		extractAuthCode: true,
+		insertTextBody: true,
+		resolveFragmentIdentifierURLs: false,
+		userScriptEnabled: false,
+		saveCreatedBookmarks: false,
+		ignoredBookmarkFolders: [],
+		replaceBookmarkURL: true,
+		saveFavicon: true,
+		includeBOM: false
+	};
+
+	this.retrievePageData = async function() {
+		try {
+			// Call to background script to inject SingleFile
+			await Zotero.Connector_Browser.injectSingleFile();
+
+			Zotero.debug("SingleFile: Retrieving page data");
+			let pageData = await singlefile.extension.getPageData(Zotero.SingleFile.CONFIG);
+			Zotero.debug("SingleFile: Done retrieving page data");
+
+			// Replace Uint8Array objects with a UUID and place the resources as top-
+			// level Uint8Array objects ready for a multipart request. We can't convert
+			// them to binary blobs yet because Chrome doesn't support moving binary
+			// blobs from the injected page to the background.
+			let form = {};
+			function convertResources(resources) {
+				Object.keys(resources).forEach((resourceType) => {
+					resources[resourceType].forEach((data, index) => {
+						// Frames have whole new set of resources
+						// We handle these by recursion
+						if (resourceType === "frames") {
+							convertResources(resources[resourceType][index].resources);
+							return;
+						}
+						// Some data is already encoded as string
+						if (typeof data.content === "string") {
+							return;
+						}
+						// For binary data, we replace the content with a
+						// UUID and create a new part for the multipart
+						// upload
+						let uuid = 'binary-' + Zotero.Utilities.randomString();
+						form[uuid] = data.content;
+						resources[resourceType][index].content = uuid;
+						resources[resourceType][index].binary = true;
+					});
+				});
+			}
+			convertResources(pageData.resources);
+			
+			Zotero.debug("SingleFile: Done encoding page data");
+			return {
+				pageData,
+				form
+			};
+		} catch (e) {
+			Zotero.debug("SingleFile: Error retrieving page data", 2);
+			Zotero.debug(e.stack, 2);
+			throw e;
+		}
+	};
+};

src/common/connector.js
@@ -151,6 +151,20 @@ Zotero.Connector = new function() {
 		if (headers["Content-Type"] == 'application/json') {
 			data = JSON.stringify(data);
 		}
+		else if (headers["Content-Type"] == 'multipart/form-data') {
+			let formData = new FormData();
+			for (const entry in data) {
+				// For SingleFile binary arrays, convert them to blobs
+				if (entry.startsWith('binary-')) {
+					const int8array = new Uint8Array(Object.values(data[entry]));
+					formData.append(entry, new Blob([int8array]));
+				}
+				else {
+					formData.append(entry, data[entry]);
+				}
+			}
+			data = formData;
+		}
 		options = {body: data, headers, successCodes: false, timeout};
 		let httpMethod = data == null || data == undefined ? "GET" : "POST";
 		Zotero.HTTP.request(httpMethod, uri, options)

src/common/http.js
@@ -132,7 +132,16 @@ Zotero.HTTP = new function() {
 				throw new Error(`HTTP ${method} cannot have a request body (${options.body})`)
 			}
 		} else if(options.body) {
-			options.body = typeof options.body == 'string' ? options.body : JSON.stringify(options.body);
+			if (options.headers["Content-Type"] !== 'multipart/form-data') {
+				options.body = typeof options.body == 'string' ? options.body : JSON.stringify(options.body);
+
+				logBody = `: ${options.body.substr(0, options.logBodyLength)}` +
+						options.body.length > options.logBodyLength ? '...' : '';
+				// TODO: make sure below does its job in every API call instance
+				// Don't display password or session id in console
+				logBody = logBody.replace(/password":"[^"]+/, 'password":"********');
+				logBody = logBody.replace(/password=[^&]+/, 'password=********');
+			}
 			
 			if (!options.headers) options.headers = {};
 			if (!options.headers["Content-Type"]) {
@@ -142,13 +151,6 @@ Zotero.HTTP = new function() {
 				// Allow XHR to set Content-Type with boundary for multipart/form-data
 				delete options.headers["Content-Type"];
 			}
-					
-			logBody = `: ${options.body.substr(0, options.logBodyLength)}` +
-					options.body.length > options.logBodyLength ? '...' : '';
-			// TODO: make sure below does its job in every API call instance
-			// Don't display password or session id in console
-			logBody = logBody.replace(/password":"[^"]+/, 'password":"********');
-			logBody = logBody.replace(/password=[^&]+/, 'password=********');
 		}
 		if (options.headers['User-Agent'] && Zotero.isBrowserExt) {
 			await Zotero.WebRequestIntercept.replaceUserAgent(url, options.headers['User-Agent']);

src/common/inject/inject.jsx
@@ -502,8 +502,10 @@ Zotero.Inject = new function() {
 			sessionID,
 			url: document.location.toString(),
 			cookie: document.cookie,
+			title: title,
 			html: document.documentElement.innerHTML,
-			skipSnapshot: !options.snapshot
+			skipSnapshot: !options.snapshot,
+			singleFile: true
 		};
 
 		var image;
@@ -519,11 +521,12 @@ Zotero.Inject = new function() {
 			"progressWindow.itemProgress",
 			{
 				sessionID,
-				id: title,
+				id: 1,
 				iconSrc: Zotero.ItemTypes.getImageSrc(image),
 				title: title
 			}
 		);
+
 		try {
 			var result = await Zotero.Connector.callMethodWithCookies("saveSnapshot", data);
 			Zotero.Messaging.sendMessage("progressWindow.sessionCreated", { sessionID });
@@ -531,13 +534,57 @@ Zotero.Inject = new function() {
 				"progressWindow.itemProgress",
 				{
 					sessionID,
-					id: title,
+					id: 1,
 					iconSrc: Zotero.ItemTypes.getImageSrc(image),
 					title,
 					parentItem: false,
 					progress: 100
 				}
 			);
+
+			// Once snapshot item is created, if requested, run SingleFile
+			if (result && result.saveSingleFile) {
+				let progressItem = {
+					sessionID,
+					id: 2,
+					iconSrc: Zotero.ItemTypes.getImageSrc("attachment-snapshot"),
+					title: "Snapshot",
+					parentItem: 1,
+					progress: 0
+				};
+
+				Zotero.Messaging.sendMessage("progressWindow.itemProgress", progressItem);
+
+				let snapshotFailed = false;
+				let payload = data;
+				try {
+					let { pageData, form } = await Zotero.SingleFile.retrievePageData();
+					payload.pageData = pageData;
+					form.payload = JSON.stringify(payload);
+					payload = form;
+				}
+				catch (e) {
+					// Swallow error, will fallback to save in client
+					payload = {
+						payload: JSON.stringify(payload)
+					};
+
+					snapshotFailed = true;
+					Zotero.Messaging.sendMessage("progressWindow.itemProgress", { ...progressItem, ...{ progress: false } });
+				}
+
+				result = await Zotero.Connector.callMethodWithCookies({
+						method: "saveSingleFile",
+						headers: {"Content-Type": "multipart/form-data"}
+					},
+					payload
+				);
+
+				if (!snapshotFailed) {
+					Zotero.Messaging.sendMessage("progressWindow.itemProgress", { ...progressItem, ...{ progress: 100 } });
+				}
+			}
+
 			Zotero.Messaging.sendMessage("progressWindow.done", [true]);
 			Object.assign(this.sessionDetails, {
 				id: sessionID,

commit cc21f88396cfce8481e4277ecffe93d4f3007296

Fixing saving on pages that included snapshots by document
Regression from 4e0437813a

src/common/translate_item.js
@@ -115,7 +115,7 @@ Zotero.Translate.ItemSaver.prototype = {
 					|| attachment.snapshot === false) {
 					continue;
 				}
-				if (!this._urlMatchesLocation(attachment.url)) {
+				if (attachment.url && !this._urlMatchesLocation(attachment.url)) {
 					continue;
 				}
 
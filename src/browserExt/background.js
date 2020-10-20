zotero/zotero-connectors

commit ef2fa4eb068ebdbf110306b2c686a680b4700814

Update unit tester and tests

src/browserExt/background.js
@@ -66,7 +66,7 @@ Zotero.Connector_Browser = new function() {
 	 * @param tabId
 	 */
 	this.onPDFFrame = function(frameURL, frameId, tabId) {
-		if (_tabInfo[tabId] && _tabInfo[tabId].translators) {
+		if (_tabInfo[tabId] && _tabInfo[tabId].translators && _tabInfo[tabId].translators.length) {
 			return;
 		}
 		browser.tabs.get(tabId).then(function(tab) {
@@ -445,6 +445,10 @@ Zotero.Connector_Browser = new function() {
 	 * Update status and tooltip of Zotero button
 	 */
 	this._updateExtensionUI = function (tab) {
+		if (!tab) {
+			return chrome.tabs.query( { lastFocusedWindow: true, active: true },
+				(tabs) => tabs.length && this._updateExtensionUI(tabs[0]));
+		}	
 		if (Zotero.Prefs.get('firstUse') && Zotero.isFirefox) return _showFirstUseUI(tab);
 		if (!tab.active) return;
 		browser.contextMenus.removeAll();
@@ -545,8 +549,7 @@ Zotero.Connector_Browser = new function() {
 	function _isDisabledForURL(url, excludeTests=false) {
 		return url.startsWith('chrome://') ||
 			url.startsWith('about:') ||
-			url.startsWith('chrome-') ||
-			(url.includes('-extension://') && (!excludeTests || !url.includes('/test/data/')));
+			(url.startsWith('chrome-') && (!excludeTests || !url.includes('/test/data/')));
 	}
 	
 	function _showZoteroStatus(tabID) {

.travis.yml
@@ -3,6 +3,8 @@ sudo: false
 language: node_js
 node_js:
     - "node"
+services:
+    - xvfb
 cache:
     directories:
         - "node_modules"
@@ -18,9 +20,5 @@ matrix:
           env: TEST_FX="1"
 notifications:
     email: false
-install:
-before_script:
-    - export DISPLAY=:99.0
-    - sh -e /etc/init.d/xvfb start
 script:
     - ./test/run_tests

src/browserExt/test/test.html
@@ -39,6 +39,7 @@
 <script src="../../promise.js"></script>
 <script src="../../messages.js"></script>
 <script src="../../messaging_inject.js"></script>
+<script src="../../i18n.js"></script>
 <script src="../../lib/bluebird.js"></script>
 <script src="../../lib/mocha.js"></script>
 <script src="../../lib/chai.js"></script>

src/common/test/tests/backgroundTest.js
@@ -47,7 +47,17 @@ describe('Connector_Browser', function() {
 				});
 				await Zotero.Promise.delay(30);
 				await tab.init(getExtensionURL('test/data/framePDF.html'));
+				
+				// The frame is not initialized unless the tab is activated momentarily.
+				await background(async function(tabId) {
+					var prevActiveTab = await browser.tabs.query({active: true, currentWindow: true});
+					await browser.tabs.update(tabId, {active: true});
+					await Zotero.Promise.delay(100);
+					await browser.tabs.update(prevActiveTab[0].id, {active: true});
+				}, tab.tabId);
+				
 				await bgPromise;
+
 				let tabId = await background(async function(tabId) {
 					if (Zotero.isBrowserExt) {
 						return Zotero.Connector_Browser._showPDFIcon.args[0][0].id;

src/common/test/tests/translationTest.js
@@ -164,13 +164,16 @@ describe("Translation", function() {
 							stub.restore(); stub2.restore();	
 						}
 					}, tab.tabId);
+					var translatorNames = await tab.run(() => {
+						return Zotero.Inject.translators.map(translator => translator.label);
+					})
 					var frameURL = getExtensionURL('progressWindow/progressWindow.html');
 					var message = await tab.runInFrame(frameURL, async function() {
 						// TODO: A more robust way to wait for the text to show up.
 						await Zotero.Promise.delay(100);
 						return document.querySelector('.ProgressWindow-error').textContent;
 					});
-					assert.include(message, 'An error occurred while saving this item.');
+					assert.include(message, Zotero.i18n.getString('progressWindow_error_fallback').substr(0, 20));
 				});
 			});
 			

test/seleniumTests.js
@@ -85,20 +85,21 @@ if ('TEST_CHROME' in process.env) {
 	(async function() {
 		try {
 			require('chromedriver');
-			let caps = selenium.Capabilities.chrome();
-			
-			let options = {'args': [`load-extension=${chromeExtensionDir}`]};
+			let chrome = require('selenium-webdriver/chrome');
+			let options = new chrome.Options();
+			options.addArguments(`load-extension=${chromeExtensionDir}`);
 			if ('BROWSER_EXECUTABLE' in process.env) {
-				options['binary'] = process.env['BROWSER_EXECUTABLE']
+				options.setChromeBinaryPath(process.env['BROWSER_EXECUTABLE']);
 			}
 
-			caps.set('chromeOptions', options);
 			let driver = new selenium.Builder()
-				.withCapabilities(caps)
+				.forBrowser('chrome')
+				.setChromeOptions(options)
 				.build();
 			
 			// No API to retrieve extension ID. Hacks, sigh.
 			await driver.get("chrome://system/");
+			await driver.wait(until.elementLocated({id: 'extensions-value-btn'}), 60*1000);
 			let extBtn = await driver.findElement({css: '#extensions-value-btn'});
 			await extBtn.click();
 			let contentElem = await driver.findElement({css: '#content'});
@@ -125,7 +126,6 @@ if ('TEST_FX' in process.env) {
 		try {
 			require('geckodriver');
 
-			
 			const firefox = require('selenium-webdriver/firefox');
 			var options = new firefox.Options();
 			if ('BROWSER_EXECUTABLE' in process.env) {
@@ -144,9 +144,13 @@ if ('TEST_FX' in process.env) {
 			`);
 			await driver.setContext(firefox.Context.CONTENT);
 
-			await driver.installAddon('/tmp/zoteroConnector.xpi');
-			await driver.get('about:debugging');
-			let elem = await driver.findElement({css: '.internal-uuid span'});
+			let uuid = await driver.installAddon('/tmp/zoteroConnector.xpi');
+			// Doing some crazy xpath matching since extId cannot be retrieved by API
+			// Sigh. This is bound to break eventually
+			let extIdXPath = `//dd[text()="${uuid}"]/../following-sibling::div/dd`;
+			await driver.get('about:debugging#/runtime/this-firefox');
+			await driver.wait(until.elementLocated({xpath: extIdXPath}), 60*1000);
+			let elem = await driver.findElement({xpath: extIdXPath});
 			let extId = await elem.getText();
 			let testUrl = `moz-extension://${extId}/test/test.html#console`;
 			await new Promise((resolve) => setTimeout(() => resolve(driver.get(testUrl)), 500));

commit 6aea5c626739e5c9e8f02900500e44bbb70bf19e

Fix target selector brokenness in non-Chrome browsers.
Closes #329. Regression in ef2fa4eb

src/browserExt/background.js
@@ -556,7 +556,7 @@ Zotero.Connector_Browser = new function() {
 	function _isDisabledForURL(url, excludeTests=false) {
 		return url.startsWith('chrome://') ||
 			url.startsWith('about:') ||
-			(url.startsWith('chrome-') && (!excludeTests || !url.includes('/test/data/')));
+			(url.startsWith(browser.runtime.getURL('')) && (!excludeTests || !url.includes('/test/data/')));
 	}
 	
 	function _showZoteroStatus(tabID) {
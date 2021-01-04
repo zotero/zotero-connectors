zotero/zotero-connectors

commit 976b2706dad3152126bb40eb18a4c16d32e28ff8

Update to Babel 7

gulpfile.js
@@ -30,7 +30,7 @@ const exec = require('child_process').exec;
 const through = require('through2');
 const gulp = require('gulp');
 const plumber = require('gulp-plumber');
-const babel = require('babel-core');
+const babel = require('@babel/core');
 const browserify = require('browserify');
 const argv = require('yargs')
 	.boolean('p')
@@ -192,7 +192,10 @@ function processFile() {
 					babel.transform(
 						file.contents,
 						{
-							plugins: ['transform-react-jsx', 'transform-class-properties']
+							plugins: [
+								'@babel/plugin-transform-react-jsx',
+								'@babel/plugin-proposal-class-properties'
+							]
 						}
 					).code
 				);

scripts/gulpfile_bookmarklet.js
@@ -31,7 +31,7 @@ const through = require('through2');
 const gulp = require('gulp');
 const plumber = require('gulp-plumber');
 const concat = require('gulp-concat');
-const babel = require('babel-core');
+const babel = require('@babel/core');
 const browserify = require('browserify');
 
 const xpcomDir = './src/zotero/chrome/content/zotero/xpcom';
@@ -157,7 +157,11 @@ function processFile(argv) { return through.obj(async function(file, enc, cb) {
 	
 	var plugins = [];
 	if (ext == 'jsx') {
-		plugins = [...plugins, 'transform-react-jsx', 'transform-class-properties'];
+		plugins = [
+			...plugins,
+			'@babel/plugin-transform-react-jsx',
+			'@babel/plugin-proposal-class-properties'
+		];
 		try {
 			file.contents = new Buffer.from(babel.transform(file.contents, { plugins }).code);
 		} catch (e) {

commit 99361f73c73d4b85d9e96365d901d731ff8cce22

Fix bookmarklet build (broken since 976b2706)

build.sh
@@ -133,43 +133,6 @@ if [[ ! -z $DEBUG ]]; then
 		"$NODE_MODULES_DIR/sinon/pkg/sinon.js")
 fi
 
-# Scripts to be included in bookmarklet
-BOOKMARKLET_INJECT_INCLUDE=("$SRCDIR/common/cachedTypes.js" \
-	"$EXTENSION_XPCOM_DIR/date.js" \
-	"$SRCDIR/common/inject/http.js" \
-	"$EXTENSION_XPCOM_DIR/openurl.js" \
-	"$EXTENSION_XPCOM_DIR/rdf/init.js" \
-	"$EXTENSION_XPCOM_DIR/rdf/uri.js" \
-	"$EXTENSION_XPCOM_DIR/rdf/term.js" \
-	"$EXTENSION_XPCOM_DIR/rdf/identity.js" \
-	"$EXTENSION_XPCOM_DIR/rdf/match.js" \
-	"$EXTENSION_XPCOM_DIR/rdf/rdfparser.js" \
-	"$EXTENSION_XPCOM_DIR/translation/translate.js" \
-	"$SRCDIR/common/translate_item.js" \
-	"$SRCDIR/common/inject/translate_inject.js" \
-	"$SRCDIR/zotero/resource/schema/connectorTypeSchemaData.js" \
-	"$EXTENSION_XPCOM_DIR/utilities_translate.js" \
-	"$SRCDIR/bookmarklet/messaging_inject.js" \
-	"$SRCDIR/bookmarklet/inject_base.js")
-
-BOOKMARKLET_IFRAME_INCLUDE=("$SRCDIR/common/connector.js" \
-	"$EXTENSION_XPCOM_DIR/translation/tlds.js" \
-	"$SRCDIR/bookmarklet/translator.js" \
-	"$SRCDIR/common/messaging.js" \
-	"$SRCDIR/bookmarklet/iframe_base.js")
-
-BOOKMARKLET_COMMON_INCLUDE=("$SRCDIR/bookmarklet/zotero_config.js" \
-	"$EXTENSION_XPCOM_DIR/debug.js" \
-	"$SRCDIR/common/errors_webkit.js" \
-	"$SRCDIR/common/http.js" \
-	"$EXTENSION_XPCOM_DIR/xregexp/xregexp.js" \
-	"$EXTENSION_XPCOM_DIR/xregexp/addons/build.js" \
-	"$EXTENSION_XPCOM_DIR/xregexp/addons/matchrecursive.js" \
-	"$EXTENSION_XPCOM_DIR/xregexp/addons/unicode/unicode-base.js" \
-	"$EXTENSION_XPCOM_DIR/xregexp/addons/unicode/unicode-categories.js" \
-	"$EXTENSION_XPCOM_DIR/xregexp/addons/unicode/unicode-zotero.js" \
-	"$EXTENSION_XPCOM_DIR/utilities.js" \
-	"$SRCDIR/bookmarklet/messages.js")
 
 BOOKMARKLET_AUXILIARY_JS=( \
 	"$SRCDIR/bookmarklet/loader.js" \
@@ -410,18 +373,6 @@ echo "done"
 
 if [ $BUILD_BOOKMARKLET == 1 ]; then
 	echo -n "Building bookmarklet..."
-	
-	# Make bookmarklet
-	
-	# Copy/minify auxiliary JS
-	if [ ! -z $DEBUG ]; then
-		cp "${BOOKMARKLET_AUXILIARY_JS[@]}" "$BUILD_DIR/bookmarklet"
-	else	
-		for scpt in "${BOOKMARKLET_AUXILIARY_JS[@]}"
-		do
-			"$CWD/node_modules/babel-cli/bin/babel.js" "$scpt" --out-file "$BUILD_DIR/bookmarklet/`basename \"$scpt\"`" --presets minify --no-comments -q >> "$LOG" 2>&1
-		done
-	fi	
 	
 	# Copy HTML to dist directory
 	cp -R "$SRCDIR/bookmarklet/debug_mode.html" \

gulpfile.js
@@ -482,7 +482,7 @@ gulp.task('process-custom-scripts', function() {
 		.pipe(gulp.dest((data) => data.base));
 });
 
-gulp.task('watch-bookmarklet', watchBookmarklet(argv))
+gulp.task('watch-bookmarklet', watchBookmarklet(argv));
 
 gulp.task('process-bookmarklet-scripts', processBookmarkletScripts(argv));
 

scripts/gulpfile_bookmarklet.js
@@ -29,10 +29,10 @@ const fs = require('fs').promises;
 const { basename, join, extname } = require('path');
 const through = require('through2');
 const gulp = require('gulp');
+const mergeStream = require('merge-stream');
 const plumber = require('gulp-plumber');
 const concat = require('gulp-concat');
 const babel = require('@babel/core');
-const browserify = require('browserify');
 const replaceBrowser = require('./replace_browser');
 
 const xpcomDir = './src/zotero/chrome/content/zotero/xpcom';
@@ -181,7 +181,7 @@ function postProcess(argv) { return through.obj(function(file, enc, cb) {
 		let presets = [];
 		// presets = [...presets, 'env'];
 		if (argv.p) {
-			presets = [...presets, 'minify'];
+			presets = [...presets, ['minify', {builtIns: false}]];
 		}
 		try {
 			let fileStr = file.contents;
@@ -195,13 +195,13 @@ function postProcess(argv) { return through.obj(function(file, enc, cb) {
 		}
 		// browserify(file).bundle((err, buf) => {file.contents = buf; this.push(file.clone({contents: false})); cb()});
 	}
-	this.push(file.clone({contents: false}));
+	this.push(file.clone({contents: false})); cb();
 })}
 
 function logFinalOutput() {
-	return through.obj(function(file) {
+	return through.obj(function(file, enc, cb) {
 		console.log(`-> ${basename(file.path)}`);
-		this.push(file);
+		this.push(file); cb();
 	})
 }
 
@@ -232,14 +232,16 @@ function watchBookmarklet(argv) {return function() {
 }}
 
 function processBookmarkletScripts(argv) {return function() {
+	let streams = [];
 	for (let key in sources) {
-		gulp.src(sources[key]).pipe(plumber())
+		streams.push(gulp.src(sources[key]).pipe(plumber())
 			.pipe(processFile(argv))
 			.pipe(concat(key))
 			.pipe(postProcess(argv))
 			.pipe(logFinalOutput())
-			.pipe(gulp.dest('./build/bookmarklet/'));
+			.pipe(gulp.dest('./build/bookmarklet/')));
 	}
+	return mergeStream(...streams);
 }}
 
 module.exports = {
c3498076ce61ae0d123bc0beb15f706f43381680
Dan Stillman <dstillman@zotero.org>
2022-05-24 16:43:51 -0400

    Enable Google Docs API if GOOGLE_DOCS_V2 environment variable is set
    So it can be enabled for beta builds

--- gulpfile.js
+++ gulpfile.js
@@ -238,10 +238,13 @@ function processFile() {
 			case 'zotero.js':
 				var contents = file.contents.toString();
 				if (!argv.p) {
-					contents = contents
-						.replace('"debug.log": false', '"debug.log": true')
-						.replace('"integration.googleDocs.useGoogleDocsAPI": false',
-							'"integration.googleDocs.useGoogleDocsAPI": true');
+					contents = contents.replace('"debug.log": false', '"debug.log": true');
+				}
+				if (!argv.p || process.env.ZOTERO_GOOGLE_DOCS_V2) {
+					contents = contents.replace(
+						'"integration.googleDocs.useGoogleDocsAPI": false',
+							'"integration.googleDocs.useGoogleDocsAPI": true'
+					);
 				}
 				contents = contents.replace(/\/\* this\.allowRepoTranslatorTester = SET IN BUILD SCRIPT \*\//,
 					`this.allowRepoTranslatorTester = ${!!process.env.ZOTERO_REPOSITORY_URL}`);

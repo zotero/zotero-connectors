zotero/zotero-connectors

commit 13c45cb6126505ff63b536843359b7e3198071c5

Refactor and improve proxy code
- Improve EZProxy detection
- Remove EZProxy proxy-via-port support since it's not even
supported by the vendor itself
- Convert proxy schema to a two-way toProper and toProxy
schemas and use EZProxy's login url for toProxy schemas
for new proxies
- Remove dotsToHyphens preference and always convert for
https urls

src/common/preferences/preferences.jsx
@@ -504,8 +504,12 @@ Zotero_Preferences.Components.ProxyPreferences = class ProxyPreferences extends 
 Zotero_Preferences.Components.Proxies = class Proxies extends React.PureComponent {
 	constructor(props) {
 		super(props);
+		let proxies = Zotero.Prefs.get('proxies.proxies').map((proxy) => {
+			proxy.toProperScheme = proxy.toProperScheme || proxy.scheme
+			return proxy;
+		});
 		this.state = {
-			proxies: Zotero.Prefs.get('proxies.proxies'),
+			proxies,
 			currentProxy: undefined,
 			currentHostIdx: -1
 		};
@@ -524,9 +528,9 @@ Zotero_Preferences.Components.Proxies = class Proxies extends React.PureComponen
 	}
 	
 	componentDidUpdate(prevProps, prevState) {
-		if (this.focusSchemeInput) {
-			this.focusSchemeInput = false;
-			this.refs.schemeInput.focus();
+		if (this.focusToProxyInput) {
+			this.focusToProxyInput = false;
+			this.refs.toProxyInput.focus();
 		}
 		else if (this.focusHostInput) {
 			this.focusHostInput = false;
@@ -563,14 +567,15 @@ Zotero_Preferences.Components.Proxies = class Proxies extends React.PureComponen
 			if (value == '+') {
 				let newProxy = {
 					id: Date.now(),
-					scheme: '%h.example.com/%p',
+					toProxyScheme: 'https://www.example.com/login?qurl=%s',
+					toProperScheme: '%h.example.com/%p',
 					autoAssociate: true,
 					hosts: []
 				};
 				newState.proxies = prevState.proxies.concat([newProxy]);
 				newState.currentProxy = newProxy;
 				newState.currentHostIdx = -1;
-				this.focusSchemeInput = true;
+				this.focusToProxyInput = true;
 				this.saveProxy(newProxy);
 			}
 			// Delete proxy
@@ -582,7 +587,7 @@ Zotero_Preferences.Components.Proxies = class Proxies extends React.PureComponen
 					...oldProxies.slice(pos + 1)
 				];
 				Zotero.Proxies.remove(prevState.currentProxy);
-				newState.currentProxy = oldProxies[pos] || oldProxies[pos - 1];
+				newState.currentProxy = newState.proxies [pos] || newState.proxies[pos - 1];
 			}
 			return newState;
 		});
@@ -590,6 +595,7 @@ Zotero_Preferences.Components.Proxies = class Proxies extends React.PureComponen
 	
 	handleSchemeChange(event) {
 		var value = event.target.value;
+		var name = event.target.name;
 		this.setState((prevState) => {
 			var newState = {};
 			var oldProxy = prevState.currentProxy;
@@ -597,7 +603,7 @@ Zotero_Preferences.Components.Proxies = class Proxies extends React.PureComponen
 				{},
 				oldProxy,
 				{
-					scheme: value,
+					[name]: value,
 					hosts: [...oldProxy.hosts]
 				}
 			);
@@ -637,7 +643,10 @@ Zotero_Preferences.Components.Proxies = class Proxies extends React.PureComponen
 			var newProxy = Object.assign({}, oldProxy);
 			// Add host to end
 			if (value == '+') {
-				newProxy.hosts = oldProxy.hosts.concat(['']);
+				let lastHostEmpty = oldProxy.hosts.length && oldProxy.hosts[oldProxy.hosts.length-1].trim().length == 0;
+				if (!lastHostEmpty) {
+					newProxy.hosts = oldProxy.hosts.concat(['']);
+				}
 				newState.currentHostIdx = newProxy.hosts.length - 1;
 				this.focusHostInput = true;
 			}
@@ -681,29 +690,29 @@ Zotero_Preferences.Components.Proxies = class Proxies extends React.PureComponen
 		}
 		
 		let currentProxy = this.state.currentProxy;
-		let multiHost = currentProxy.scheme.indexOf('%h') != -1;
+		var multiHost = currentProxy && currentProxy.toProperScheme.includes('%h') || currentProxy.toProperScheme.includes('%u');
 		
 		// If a host exists in the last position and is empty, don't allow adding more
-		let disableAddHost = currentProxy.hosts.length && currentProxy.hosts[currentProxy.hosts.length-1].trim().length == 0;
 		let disableRemoveHost = this.state.currentHostIdx == -1;
 		
 		return (
 			<div className="group" style={{marginTop: "10px"}}>
 				<p style={{display: "flex", alignItems: "center", flexWrap: "wrap"}}>
 					<label style={{visibility: multiHost ? null : 'hidden'}}><input type="checkbox" name="autoAssociate" onChange={this.handleCheckboxChange} checked={currentProxy.autoAssociate}/>&nbsp;Automatically associate new hosts</label>
-					<label><input type="checkbox" name="dotsToHyphens" onChange={this.handleCheckboxChange} checked={currentProxy.dotsToHyphens}/>&nbsp;Automatically convert between dots and hyphens in proxied hostnames</label>
 				</p>
 				<p style={{display: "flex", alignItems: "center"}}>
-					<label style={{alignSelf: "center", marginRight: "5px"}}>Scheme: </label>
-					<input style={{flexGrow: "1"}} type="text" name="scheme" onChange={this.handleSchemeChange} value={currentProxy.scheme} ref={"schemeInput"}/>
+					<label style={{alignSelf: "center", marginRight: "5px"}}>Login URL Scheme: </label>
+					<input style={{flexGrow: "1"}} type="text" name="toProxyScheme" onChange={this.handleSchemeChange} value={currentProxy.toProxyScheme} ref={"toProxyInput"}/>
+				</p>	
+				<p style={{display: "flex", alignItems: "center"}}>
+					<label style={{alignSelf: "center", marginRight: "5px"}}>Reverse-Proxy Scheme: </label>
+					<input style={{flexGrow: "1"}} type="text" name="toProperScheme" onChange={this.handleSchemeChange} value={currentProxy.toProperScheme} ref={"toProperInput"}/>
 				</p>
 				<p>
-					You may use the following variables in your proxy scheme:<br/>
+					You may use the following variables in your proxy schemes:<br/>
 					&#37;h - The hostname of the proxied site (e.g., www.example.com)<br/>
 					&#37;p - The path of the proxied page excluding the leading slash (e.g., about/index.html)<br/>
-					&#37;d - The directory path (e.g., about/)<br/>
-					&#37;f - The filename (e.g., index.html)<br/>
-					&#37;a - Any string
+					&#37;u - Full encoded proxied site url (e.g. https://www.example.com)
 				</p>
 				
 				<div style={{display: "flex", flexDirection: "column", marginTop: "10px"}}>
@@ -715,7 +724,7 @@ Zotero_Preferences.Components.Proxies = class Proxies extends React.PureComponen
 							<option key={i} value={i}>{host}</option>)}
 					</select>
 					<p>
-						<input style={{minWidth: "80px", marginRight: "10px"}} type="button" onClick={this.handleHostButtonClick} disabled={disableAddHost} value="+"/>
+						<input style={{minWidth: "80px", marginRight: "10px"}} type="button" onClick={this.handleHostButtonClick} value="+"/>
 						<input style={{minWidth: "80px", marginRight: "10px"}} type="button" onClick={this.handleHostButtonClick} disabled={disableRemoveHost} value="-"/>
 					</p>
 
@@ -739,7 +748,7 @@ Zotero_Preferences.Components.Proxies = class Proxies extends React.PureComponen
 						onChange={this.handleProxySelectChange}
 						disabled={!this.props.transparent}>
 					{this.state.proxies.length && this.state.proxies.map((proxy, i) => {
-						return <option key={i} value={proxy.id}>{proxy.scheme}</option>;
+						return <option key={i} value={proxy.id}>{proxy.toProxyScheme || proxy.toProperScheme}</option>;
 					})}
 				</select>
 				<p style={{display: this.props.transparent ? null : 'none'}}>

commit ad8f5d3bb2e2098b59390981bac24de2f3642914

Proxies: add more sophisticated proxy redirection loop prevention
Regression from 13c45cb6
Reports in https://forums.zotero.org/discussion/102507/zotero-causing-continuous-article-download-request-loop

Changes in 13c45cb6 made us redirect to ezproxy resources via the
/login?url= links which is the appropriate way to redirect via proxy
instead of manually rewriting urls to their final form however this type
of redirection causes multiple requests and so the unsophisticated
single-request proxy loop prevention code was no longer sufficient.

This commit adds a more sophisticated system that watches tabs after
redirection to ensure they don't end up navigating back to the original
URL for a set number of navigation events. If the tab ends up looping we
add a temporary block to proxy redirection and show a message in
preferences with a button to reenable automatic redirection.

src/common/messages.js
@@ -220,7 +220,8 @@ var MESSAGES = {
 	Proxies: {
 		loadPrefs: false,
 		save: false,
-		remove: false
+		remove: false,
+		toggleRedirectLoopPrevention: false
 	},
 	Repo: {
 		update: false

src/common/preferences/preferences.jsx
@@ -438,7 +438,7 @@ Zotero_Preferences.Components.ProxyPreferences = class ProxyPreferences extends 
 		super(props);
 		let state = {};
 		let settings = ['transparent', 'autoRecognize', 'showRedirectNotification',
-			 'disableByDomain', 'disableByDomainString'];
+			 'disableByDomain', 'disableByDomainString', 'loopPreventionTimestamp'];
 		for (let setting of settings) {
 			state[setting] = Zotero.Prefs.get('proxies.'+setting);
 		}
@@ -454,6 +454,11 @@ Zotero_Preferences.Components.ProxyPreferences = class ProxyPreferences extends 
 	
 	handleTextInputChange(event) {
 		this.updateState(event.target.name, event.target.value);
+	}
+	
+	reenableProxyRedirection = () => {
+		Zotero.Proxies.toggleRedirectLoopPrevention(false);
+		this.setState({ loopPreventionTimestamp: 0 });
 	}
 	
 	updateState(name, value) {
@@ -473,18 +478,29 @@ Zotero_Preferences.Components.ProxyPreferences = class ProxyPreferences extends 
 		if (Zotero.isBrowserExt) {
 			autoRecognise = <label><input type="checkbox" disabled={!this.state.transparent} onChange={this.handleCheckboxChange} name="autoRecognize" defaultChecked={this.state.autoRecognize}/>&nbsp;Automatically detect new proxies</label>;
 		}
+		let redirectLoopPrevention = ''
+		if (this.state.loopPreventionTimestamp > Date.now() && this.state.transparent) {
+			redirectLoopPrevention = (
+				<div className="group">
+					<b>Zotero detected a proxy redirect loop and has temporarily suspended automatic proxy redirection</b> <input type="button" onClick={this.reenableProxyRedirection} value="Reenable proxy redirection"/>
+				</div>
+			)
+		}
 		return (
 			<div>
-				<label><input type="checkbox" name="transparent" onChange={this.handleCheckboxChange} defaultChecked={this.state.transparent}/>&nbsp;Enable proxy redirection</label>
-				<div style={{marginLeft: "1em"}}>
-					<label><input type="checkbox" disabled={!this.state.transparent} onChange={this.handleCheckboxChange} name="showRedirectNotification" defaultChecked={this.state.showRedirectNotification}/>&nbsp;Show a notification when redirecting through a proxy</label>
-					{autoRecognise}
-					<p>
-						<label><input type="checkbox" disabled={!this.state.transparent} onChange={this.handleCheckboxChange} name="disableByDomain" defaultChecked={this.state.disableByDomain}/>&nbsp;Disable proxy redirection when my domain name contains<span>*</span></label>
-						<input style={{marginTop: "0.5em", marginLeft: "1.5em"}} type="text" onChange={this.handleTextInputChange} disabled={!this.state.transparent || !this.state.disableByDomain} name="disableByDomainString" defaultValue={this.state.disableByDomainString}/>
-					</p>
+				{redirectLoopPrevention}
+				<div>
+					<label><input type="checkbox" name="transparent" onChange={this.handleCheckboxChange} defaultChecked={this.state.transparent}/>&nbsp;Enable proxy redirection</label>
+					<div style={{marginLeft: "1em"}}>
+						<label><input type="checkbox" disabled={!this.state.transparent} onChange={this.handleCheckboxChange} name="showRedirectNotification" defaultChecked={this.state.showRedirectNotification}/>&nbsp;Show a notification when redirecting through a proxy</label>
+						{autoRecognise}
+						<p>
+							<label><input type="checkbox" disabled={!this.state.transparent} onChange={this.handleCheckboxChange} name="disableByDomain" defaultChecked={this.state.disableByDomain}/>&nbsp;Disable proxy redirection when my domain name contains<span>*</span></label>
+							<input style={{marginTop: "0.5em", marginLeft: "1.5em"}} type="text" onChange={this.handleTextInputChange} disabled={!this.state.transparent || !this.state.disableByDomain} name="disableByDomainString" defaultValue={this.state.disableByDomainString}/>
+						</p>
+					</div>
+					<p><span>*</span>Available when Zotero is running</p>
 				</div>
-				<p><span>*</span>Available when Zotero is running</p>
 			</div>
 		);
 	}

src/common/proxy.js
@@ -47,6 +47,7 @@
  * @property hosts {Zotero.Proxy{}} Object mapping hosts to proxies
  */
 Zotero.Proxies = new function() {
+	this.PROXY_REDIRECTION_LOOP_TIMEOUT = 3600e3;
 	/**
 	 * Initializes the proxy settings
 	 * @returns Promise{boolean} proxy enabled/disabled status
@@ -56,6 +57,8 @@ Zotero.Proxies = new function() {
 		this.transparent = false;
 		this.proxies = [];
 		this.hosts = {};
+		this._redirectedTabIDs = {};
+		this._loopPreventionTimestamp = Zotero.Prefs.get('proxies.loopPreventionTimestamp');
 		
 		this.loadPrefs();
 		
@@ -95,6 +98,7 @@ Zotero.Proxies = new function() {
 	this.enable = function() {
 		if (Zotero.isBrowserExt) {
 			Zotero.WebRequestIntercept.addListener('headersReceived', Zotero.Proxies.onWebRequest);
+			browser.webNavigation.onCommitted.addListener(Zotero.Proxies._checkForRedirectLoop)
 		} else {
 			safari.application.addEventListener('beforeNavigate', this.onBeforeNavigateSafari, false);
 		}
@@ -104,6 +108,7 @@ Zotero.Proxies = new function() {
 	this.disable = function() {
 		if (Zotero.isBrowserExt) {
 			Zotero.WebRequestIntercept.removeListener('headersReceived', Zotero.Proxies.onWebRequest);
+			browser.webNavigation.onCommitted.removeListener(Zotero.Proxies._checkForRedirectLoop)
 		} else {
 			safari.application.removeEventListener('beforeNavigate', this.onBeforeNavigateSafari, false);
 		}
@@ -184,12 +189,13 @@ Zotero.Proxies = new function() {
 	 *
 	 * @param {Object} details - webRequest details object
 	 */
-	this.onWebRequest = function (details, meta) {
-		if (meta.proxyRedirected || details.statusCode >= 400
-			|| details.frameId != 0) {
+	this.onWebRequest = (details, meta) => {
+		if (details.statusCode >= 400 || details.frameId != 0) {
 			return;
 		}
 
+		Zotero.Proxies._checkForRedirectLoop(details);
+		
 		Zotero.Proxies._maybeAddHost(details);
 		
 		if (Zotero.Proxies.autoRecognize) {
@@ -198,13 +204,51 @@ Zotero.Proxies = new function() {
 
 		Zotero.Proxies.updateDisabledByDomain();
 		if (Zotero.Proxies.disabledByDomain) return;
-
+		
+		if (Zotero.Proxies.isPreventingRedirectLoops()) return;
+		
 		let redirect = Zotero.Proxies._maybeRedirect(details);
 		if (redirect) {
-			meta.proxyRedirected = true;
+			// We will track this tab in case it causes a redirect loop
+			this._redirectedTabIDs[details.tabId] = { host: new URL(details.url).host, count: 4};
 			browser.tabs.update(details.tabId, {url: redirect.redirectUrl});
 		}
 	};
+	
+	this.toggleRedirectLoopPrevention = function(value) {
+		if (typeof value === "undefined") {
+			Zotero.debug('Called Proxies._toggleRedirectLoopPrevention() without an argument');
+		}
+		Zotero.debug(`Proxies: ${value ? "Enabling" : "Disabling"} loop prevention`)
+		this._loopPreventionTimestamp = value ? (Date.now() + this.PROXY_REDIRECTION_LOOP_TIMEOUT) : 0;
+		Zotero.Prefs.set('proxies.loopPreventionTimestamp', this._loopPreventionTimestamp)
+	}
+	
+	this.isPreventingRedirectLoops = function() {
+		return this._loopPreventionTimestamp > Date.now();
+	}
+	
+	this._checkForRedirectLoop = (details) => {
+		if (details.frameId != 0) return;
+		let redirectedTab = this._redirectedTabIDs[details.tabId];
+		if (redirectedTab) {
+			if (details.transitionQualifiers && details.transitionQualifiers.includes('forward_back')) {
+				// History navigation (back button) may cause false-positives here, so we stop monitoring
+				delete this._redirectedTabIDs[details.tabId];
+			}
+			else if (redirectedTab.host == new URL(details.url).host) {
+				// Rerouted back to the original host after a redirect, so likely there is a redirect loop
+				this.toggleRedirectLoopPrevention(true);
+				delete this._redirectedTabIDs[details.tabId];
+				return true;
+			}
+			else if (redirectedTab.count-- <= 0) {
+				// Did not detect a redirect loop on this host
+				delete this._redirectedTabIDs[details.tabId];
+			}
+		}
+		return false;
+	}
 	
 	this._maybeAddHost = function(details) {
 		// see if there is a proxy we already know
@@ -250,6 +294,7 @@ Zotero.Proxies = new function() {
 			}
 			proxy.hosts.push(host);
 			Zotero.Proxies.save(proxy);
+			Zotero.toggleRedirectLoopPrevention(false);
 
 			_showNotification(
 				'New Zotero Proxy Host',

src/common/zotero.js
@@ -415,6 +415,7 @@ Zotero.Prefs = new function() {
 		"proxies.disableByDomainString": '.edu',
 		"proxies.proxies": [],
 		"proxies.clientChecked": false,
+		"proxies.loopPreventionTimestamp": 0,
 		
 		"integration.googleDocs.enabled": true,
 		"integration.googleDocs.useGoogleDocsAPI": false,
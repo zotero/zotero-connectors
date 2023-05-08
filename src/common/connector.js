zotero/zotero-connectors

commit b29a6a52e412a6e9c84c72c986ec987965ddc76e

Fix error log submission in MV3

src/common/messages.js
@@ -121,6 +121,7 @@ var MESSAGES = {
 		}
 	},
 	Debug: {
+		get: true,
 		bgInit: false,
 		clear: false,
 		log: {
@@ -156,12 +157,10 @@ var MESSAGES = {
 		storing: true,
 		get: true,
 		count: true,
-		submitReport: true
 	},
 	Errors: {
 		log: false,
 		getErrors: true,
-		sendErrorReport: true
 	},
 	Messaging: {
 		sendMessage: {
@@ -267,7 +266,7 @@ MESSAGES.COHTTP = {
 	}
 };
 
-if(Zotero.isSafari) {
+if (Zotero.isSafari) {
 	MESSAGES.API.createItem = true;
 	MESSAGES.API.uploadAttachment = false;
 	MESSAGES.i18n = {
@@ -280,9 +279,6 @@ if(Zotero.isSafari) {
 		onTabData: true,
 		getExtensionVersion: true
 	});
-	MESSAGES.Debug.get = true;
-	delete MESSAGES.Errors.sendErrorReport;
-	delete MESSAGES.Connector_Debug.submitReport;
 }
 
 // Chrome does not support passing arrayBuffers via the message

src/common/zotero.js
@@ -347,7 +347,7 @@ var Zotero = global.Zotero = new function() {
 		}
 		
 		if(fileName && lineNumber) {
-			console.error(err+" at "+fileName+":"+lineNumber);
+			console.error(message+" at "+fileName+":"+lineNumber);
 		} else {
 			console.error(err);
 		}

commit 894a1e47a1801da15f99cd530aaf99187e469471

Fix report logs not including extensions
Regressions in b29a6a52, a18638f

src/common/connector.js
@@ -315,7 +315,7 @@ Zotero.Connector_Debug = new function() {
 	 */
 	this.submitReport = async function() {
 		let body = await Zotero.Debug.get();
-		let sysInfo = JSON.parse(await Zotero.getSystemInfo());
+		let sysInfo = JSON.parse(await Zotero.Errors.getSystemInfo());
 		let errors = (await Zotero.Errors.getErrors()).join('\n');
 		sysInfo.timestamp = new Date().toString();
 		body = `${errors}\n\n${JSON.stringify(sysInfo, null, 2)}\n\n${body}`;

src/common/errors_webkit.js
@@ -61,6 +61,45 @@ Zotero.Errors = new function() {
 	this.getErrors = async function() {
 		return _output.slice();
 	}
+
+	/**
+	 * Get versions, platform, etc.
+	 */
+	this.getSystemInfo = async function () {
+		var info;
+		if (Zotero.isSafari && Zotero.isBackground) {
+			info = {
+				connector: "true",
+				version: this.version,
+				platform: "Safari App Extension",
+			};
+		} else {
+			info = {
+				connector: "true",
+				version: this.version,
+				platform: navigator.platform,
+				locale: navigator.language,
+				userAgent: navigator.userAgent
+			};
+		}
+		
+		info.appName = Zotero.appName;
+		info.zoteroAvailable = !!(await Zotero.Connector.checkIsOnline());
+		
+		
+		if (Zotero.isBackground && Zotero.isBrowserExt) {
+			let granted = await browser.permissions.contains({permissions: ['management']});
+			if (granted) {
+				let extensions = await browser.management.getAll();
+				info.extensions = extensions
+					.filter(extension => extension.enabled && extension.name != Zotero.appName)
+					.map(extension => {
+						return `${extension.name}: ${extension.version}, ${extension.type}`;
+					}).join(', ')
+			}
+		}
+		return JSON.stringify(info, null, 2);
+	}
 	
 	/**
 	 * Sends an error report to the server
@@ -69,7 +108,7 @@ Zotero.Errors = new function() {
 	 * in the global page
 	 */
 	this.sendErrorReport = async function() {
-		var info = await Zotero.getSystemInfo();
+		var info = await this.getSystemInfo();
 		var parts = {
 			error: "true",
 			errorData: (await this.getErrors()).join('\n'),

src/common/messages.js
@@ -161,6 +161,7 @@ var MESSAGES = {
 	Errors: {
 		log: false,
 		getErrors: true,
+		getSystemInfo: true,
 	},
 	Messaging: {
 		sendMessage: {

src/common/preferences/preferences.jsx
@@ -303,7 +303,7 @@ Zotero_Preferences.Advanced = {
 		toggleDisabled(submitOutputButton, true);
 
 		// We have to request permissions within a user gesture (even though we use this in Zotero.getSystemInfo())
-		if (Zotero.browserExt) {
+		if (Zotero.isBrowserExt) {
 			try {
 				await browser.permissions.request({permissions: ['management']});
 			} catch (e) {
@@ -338,7 +338,7 @@ Zotero_Preferences.Advanced = {
 		toggleDisabled(reportErrorsButton, true);
 		
 		// We have to request permissions within a user gesture (even though we use this in Zotero.getSystemInfo())
-		if (Zotero.browserExt) {
+		if (Zotero.isBrowserExt) {
 			try {
 				await browser.permissions.request({permissions: ['management']});
 			} catch (e) {

src/common/zotero.js
@@ -281,44 +281,7 @@ var Zotero = global.Zotero = new function() {
 		Zotero.Date.init(dateFormatsJSON);
 	};
 
-	/**
-	 * Get versions, platform, etc.
-	 */
-	this.getSystemInfo = async function() {
-		var info;
-		if (Zotero.isSafari && Zotero.isBackground) {
-			info = {
-				connector: "true",
-				version: this.version,
-				platform: "Safari App Extension",
-			};
-		} else {
-			info = {
-				connector: "true",
-				version: this.version,
-				platform: navigator.platform,
-				locale: navigator.language,
-				userAgent: navigator.userAgent
-			};
-		}
-		
-		info.appName = Zotero.appName;
-		info.zoteroAvailable = !!(await Zotero.Connector.checkIsOnline());
-		
-		
-		if (Zotero.isBackground && Zotero.isBrowserExt) {
-			let granted = await browser.permissions.contains({permissions: ['management']});
-			if (granted) {
-				let extensions = await browser.management.getAll();
-				info.extensions = extensions
-					.filter(extension => extension.enabled && extension.name != Zotero.appName)
-					.map(extension => {
-						return `${extension.name}: ${extension.version}, ${extension.type}`;
-					}).join(', ')
-			}
-		}
-		return JSON.stringify(info, null, 2);
-	};
+	this.getSystemInfo = Zotero.Errors.getSystemInfo;
 	
 	/**
 	 * Writes a line to the debug console
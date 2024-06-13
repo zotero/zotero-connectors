zotero/zotero-connectors

commit 8007d9ad2aa57c947865ec6a4ed59069aff6807c

MV3: Fix translation sandbox frame breaking page layouts on some sites
Closes #479

src/browserExt/zoteroFrame.js
@@ -39,10 +39,9 @@ class ZoteroFrame {
 			id: Zotero.Utilities.randomString()
 		}, attributes);
 		this._frame = document.createElement("iframe");
-		for (let key in attributes) {
-			this._frame[key] = attributes[key];
-		}
-		this._frame.setAttribute("frameborder", "0");
+		
+		this._setFrameAttributes(attributes, style);
+
 
 		if (!messagingOptions) {
 			this.initializedPromise = Zotero.Promise.resolve();
@@ -59,6 +58,23 @@ class ZoteroFrame {
 		}
 		
 		document.body?.appendChild(this._frame);
+		
+		// Some websites (peda.net) run code that changes our iframe styling
+		// and in the case of the translation sandbox
+		// making it visible, so we need to be waiting for that and change it back.
+		let observer = new MutationObserver(() => {
+			observer.disconnect();
+			this._setFrameAttributes(attributes, style);
+			observer.observe(this._frame, { attributes: true })
+		});
+		observer.observe(this._frame, { attributes: true })
+	}
+	
+	_setFrameAttributes(attributes, style) {
+		for (let key in attributes) {
+			this._frame[key] = attributes[key];
+		}
+		this._frame.setAttribute("frameborder", "0");
 		for (let key in style) {
 			if (this._frame.style) {
 				this._frame.style[key] = style[key];

commit b5cf8a0f326fc78ba4abcfa90b4cc9efd0a3ed57

MV3: Prevent Zotero Connector from breaking SingleFile
Regression from 8007d9ad

src/browserExt/zoteroFrame.js
@@ -36,12 +36,12 @@ class ZoteroFrame {
 	constructor(attributes={}, style={}, messagingOptions) {
 		if (!attributes.src) throw new Error("Attempted to construct a Zotero frame with no src attribute");
 		attributes = Object.assign({
-			id: Zotero.Utilities.randomString()
+			id: Zotero.Utilities.randomString(),
+			frameborder: "0"
 		}, attributes);
 		this._frame = document.createElement("iframe");
 		
 		this._setFrameAttributes(attributes, style);
-
 
 		if (!messagingOptions) {
 			this.initializedPromise = Zotero.Promise.resolve();
@@ -72,11 +72,12 @@ class ZoteroFrame {
 	
 	_setFrameAttributes(attributes, style) {
 		for (let key in attributes) {
-			this._frame[key] = attributes[key];
+			if (this._frame.getAttribute(key) !== attributes[key]) {
+				this._frame.setAttribute(key, attributes[key]);
+			}
 		}
-		this._frame.setAttribute("frameborder", "0");
 		for (let key in style) {
-			if (this._frame.style) {
+			if (this._frame.style[key] !== style[key]) {
 				this._frame.style[key] = style[key];
 			}
 		}
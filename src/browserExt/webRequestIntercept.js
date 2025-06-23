zotero/zotero-connectors

commit d2486c05d761661765555bc0b28281aabe05268d

Fetch attachments in Connector and pass to Zotero on save (#535)
And saving-to-server rework with attachment saving via connector

src/browserExt/webRequestIntercept.js
@@ -151,16 +151,7 @@ Zotero.WebRequestIntercept = {
 	
 	replaceHeaders: async function(url, headers) {
 		if (!Zotero.isBrowserExt) return;
-		if (Zotero.isManifestV3 && Zotero.isChromium) return this.replaceHeadersDNR(url, headers);
-			function headerReplacer(details) {
-				if (details.url === url) {
-					browser.webRequest.onBeforeSendHeaders.removeListener(headerReplacer);
-					Zotero.debug(`Replacing headers for ${url} to ${JSON.stringify(headers)}`);
-					headers = Object.assign(details.requestHeaders, headers);
-					return { requestHeaders: headers };
-				}
-			}
-		browser.webRequest.onBeforeSendHeaders.addListener(headerReplacer, {urls: ['<all_urls>'], types: ['xmlhttprequest']}, ['blocking', 'requestHeaders']);
+		return this.replaceHeadersDNR(url, headers);
 	},
 	
 	replaceHeadersDNR: async function(url, headers) {
@@ -176,7 +167,7 @@ Zotero.WebRequestIntercept = {
 			},
 			condition: {
 				resourceTypes: ['xmlhttprequest'],
-				initiatorDomains: [chrome.runtime.id],
+				initiatorDomains: [new URL(browser.runtime.getURL('')).hostname],
 			}
 		}];
 		try {

src/common/http.js
@@ -25,10 +25,7 @@
 
 const MAX_BACKOFF = 64e3;
 
-let HEADERS_SPECIAL_HANDLING = ['User-Agent'];
-if (Zotero.isChromium) {
-	HEADERS_SPECIAL_HANDLING.push('Referer');
-}
+const HEADERS_SPECIAL_HANDLING = ['User-Agent', 'Cookie', 'Referer'];
 
 /**
  * Functions for performing HTTP requests, both via XMLHTTPRequest and using a hidden browser
@@ -76,6 +73,8 @@ Zotero.HTTP = new function() {
 	 *         <li>successCodes - HTTP status codes that are considered successful, or FALSE to allow all</li>
 	 *         <li>maxBackoff - how many times should a HTTP 429 backoff be attempted before the request fails
 	 *         		[default 0]</li>
+	 *         <li>forceInject - force the request to be sent via the inject script, even if it's not a same-origin request
+	 *         		[default false]</li>
 	 *     </ul>
 	 * @return {Promise<XMLHttpRequest>} A promise resolved with the XMLHttpRequest object if the
 	 *     request succeeds, or rejected if the browser is offline or a non-2XX status response
@@ -94,6 +93,7 @@ Zotero.HTTP = new function() {
 			successCodes: null,
 			maxBackoff: 10,
 			backoff: 0,
+			forceInject: false,
 		}, options);
 		let originalOptions = Zotero.Utilities.deepCopy(options);
 		
@@ -142,7 +142,7 @@ Zotero.HTTP = new function() {
 		// the background page since we're unable to replace user-agent via an on-page xhr and
 		// since user-agent option is explicitly set, it takes priority.
 		let sameOriginRequestViaSafari = Zotero.isSafari && Zotero.HTTP.isSameOrigin(url) && !options.headers['User-Agent'];
-		if (Zotero.isInject && !sameOriginRequestViaSafari) {
+		if (Zotero.isInject && !options.forceInject && !sameOriginRequestViaSafari) {
 			// The privileged XHR that Firefox makes available to content scripts so that they
 			// can make cross-domain requests doesn't include the Referer header in requests [1],
 			// so sites that check for it don't work properly. As long as we're not making a
@@ -194,7 +194,7 @@ Zotero.HTTP = new function() {
 			if (options.body != null) {
 				throw new Error(`HTTP ${method} cannot have a request body (${options.body})`)
 			}
-		} else if(options.body) {
+		} else if(options.body && !(options.body instanceof ArrayBuffer)) {
 			if (options.headers["Content-Type"] !== 'multipart/form-data') {
 				options.body = typeof options.body == 'string' ? options.body : JSON.stringify(options.body);
 
@@ -217,6 +217,9 @@ Zotero.HTTP = new function() {
 		}	
 		
 		if (Zotero.isBackground) {
+			if (options.referrer) {
+				options.headers['Referer'] = options.referrer;
+			}
 			let replaceHeaders = HEADERS_SPECIAL_HANDLING.filter(header => !!options.headers[header])
 				.map(header => {
 					const val = { name: header, value: options.headers[header] }
@@ -309,7 +312,7 @@ Zotero.HTTP = new function() {
 				if (options.body != null) {
 					throw new Error(`HTTP ${method} cannot have a request body (${options.body})`)
 				}
-			} else if(options.body) {
+			} else if (options.body && !(options.body instanceof ArrayBuffer)) {
 				if (options.headers["Content-Type"] !== 'multipart/form-data') {
 					options.body = typeof options.body == 'string' ? options.body : JSON.stringify(options.body);
 
@@ -347,17 +350,16 @@ Zotero.HTTP = new function() {
 			let replaceHeaders = HEADERS_SPECIAL_HANDLING.filter(header => !!options.headers[header])
 				.map(header => {
 					const val = { name: header, value: options.headers[header] }
-					delete options.headers['User-Agent'];
+					delete options.headers[header];
 					return val;
 				});
 			if (replaceHeaders.length) {
 				DNRRuleID = await Zotero.WebRequestIntercept.replaceHeaders(url, replaceHeaders);
 			}
-			let headers = new Headers(options.headers);
 			try {
 				let fetchOptions = {
 					method,
-					headers,
+					headers: options.headers,
 					body: options.body,
 					credentials: Zotero.isInject ? 'same-origin' : 'include',
 				}

src/common/zotero.js
@@ -187,7 +187,7 @@ var Zotero = global.Zotero = new function() {
 	this.initGlobal = async function() {
 		await Zotero.Errors.init();
 		if (Zotero.isManifestV3) {
-			Zotero.logError(`Service worker (re)started at ${Zotero.Date.dateToSQL(new Date())}`);
+			Zotero.Errors.logServiceWorkerStarts(Zotero.Date.dateToSQL(new Date()));
 		}
 		Zotero.isBackground = true;
 		

gulpfile.js
@@ -128,6 +128,7 @@ var backgroundInclude = [
 	'translate/debug.js',
 	'translate/tlds.js',
 	'translate/translator.js',
+	'itemSaver_background.js',
 	'translators.js',
 	'cachedTypes.js',
 	'errors_webkit.js',
@@ -150,6 +151,7 @@ var backgroundIncludeBrowserExt = ['browser-polyfill.js'].concat(backgroundInclu
 	'contentTypeHandler.js',
 	'saveWithoutProgressWindow.js',
 	'messagingGeneric.js',
+	'browserAttachmentMonitor/browserAttachmentMonitor.js',
 	'offscreen/offscreenFunctionOverrides.js', 'background/offscreenManager.js',
 ]);
 

src/browserExt/background.js
@@ -1024,7 +1024,7 @@ Zotero.Connector_Browser = new function() {
 		);
 	}
 	
-	this.saveAsWebpage = function(tab, frameId, options) {
+	this.saveAsWebpage = function(tab, frameId, options={}) {
 		let tabInfo = this.getTabInfo(tab.id);
 		if (tabInfo.uninjectable) {
 			return Zotero.Utilities.saveWithoutProgressWindow(tab, frameId);

src/browserExt/browserAttachmentMonitor/browserAttachmentMonitor.html
@@ -1,0 +1,38 @@
+<!DOCTYPE html>
+<!--
+    ***** BEGIN LICENSE BLOCK *****
+    
+    Copyright © 2025 Corporation for Digital Scholarship
+                     Vienna, Virginia, USA
+					http://zotero.org
+    
+    This file is part of Zotero.
+    
+    Zotero is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    Zotero is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    
+    You should have received a copy of the GNU Affero General Public License
+    along with Zotero.  If not, see <http://www.gnu.org/licenses/>.
+    
+    ***** END LICENSE BLOCK *****
+-->
+
+<!-- This is just a starting point so we can detect the iframe where we'll attempt to fetch the attachment 
+	and then we redirect to the actual attachment url -->
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<script src="../browser-polyfill.js"></script>
+	<script src="browserAttachmentMonitor_inject.js"></script>
+</head>
+<body>
+</body>
+</html>

src/browserExt/browserAttachmentMonitor/browserAttachmentMonitor.js
@@ -1,0 +1,187 @@
+/*
+	***** BEGIN LICENSE BLOCK *****
+	
+	Copyright © 2025 Corporation for Digital Scholarship
+					Vienna, Virginia, USA
+					http://zotero.org
+	
+	This file is part of Zotero.
+	
+	Zotero is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	Zotero is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with Zotero.  If not, see <http://www.gnu.org/licenses/>.
+	
+	***** END LICENSE BLOCK *****
+*/
+
+Zotero.BrowserAttachmentMonitor = {
+	_downloads: new Map(),
+	_nextRuleId: 50000,
+	
+	init: function() {
+		// browser.declarativeNetRequest.onRuleMatchedDebug.addListener((details) => {
+		// 	console.log("DNR rule matched:", details);
+		// });
+		this._webRequestListeners = new Map();
+	},
+
+	waitForAttachment: async function(tabId, timeoutMs=60000) {
+		const ruleId = this._nextRuleId++;
+
+		// Create promise that will resolve when attachment is found
+		let resolveSucceeded;
+		let rejectFailed;	
+		const successPromise = new Promise((resolve, reject) => {
+			resolveSucceeded = resolve;
+			rejectFailed = reject;
+		});
+
+		// Setup message listener for success notification
+		const messageListener = (message, sender) => {
+			if (sender.tab.id === tabId 
+				&& message.type === 'attachment-monitor-loaded'
+				&& message.success) {
+					Zotero.debug(`BrowserAttachmentMonitor: Attachment successfully loaded for tab: ${tabId}`);
+					resolveSucceeded(message.success);
+					this._cleanup(ruleId, messageListener, tabRemovedListener);
+			}
+		};
+
+		// Setup tab removal listener for this specific tab
+		const tabRemovedListener = (removedTabId) => {
+			if (removedTabId === tabId) {
+				this._cleanup(ruleId, messageListener, tabRemovedListener);
+				rejectFailed(new Error(`Tab removed: ${tabId}`));
+			}
+		};
+		browser.runtime.onMessage.addListener(messageListener);
+		browser.tabs.onRemoved.addListener(tabRemovedListener);
+
+		try {
+			// Add DNR rule and wait for success or timeout
+			if (Zotero.isFirefox) {
+				await this._addWebRequestRule(tabId, ruleId);
+			} else {
+				await this._addDNRRule(tabId, ruleId);
+			}
+			
+			return await Promise.race([
+				successPromise,
+				Zotero.Promise.delay(timeoutMs).then(() => { 
+					Zotero.debug(`Attachment monitor timed out after ${timeoutMs} ms for tab: ${tabId}`);
+					throw new Error('Attachment monitor timed out'); 
+				})
+			]);
+		}
+		finally {
+			this._cleanup(ruleId, messageListener, tabRemovedListener);
+		}
+	},
+
+	_cleanup: async function(ruleId, messageListener, tabRemovedListener) {
+		// Remove listeners
+		if (messageListener) {
+			browser.runtime.onMessage.removeListener(messageListener);
+		}
+		if (tabRemovedListener) {
+			browser.tabs.onRemoved.removeListener(tabRemovedListener);
+		}
+
+		// Remove web request listeners
+		if (ruleId) {
+			if (Zotero.isFirefox) {
+				const listener = this._webRequestListeners.get(ruleId);
+				if (listener) {
+					browser.webRequest.onHeadersReceived.removeListener(listener);
+				}
+				this._webRequestListeners.delete(ruleId);
+			}
+			else {
+				await browser.declarativeNetRequest.updateSessionRules({
+					removeRuleIds: [ruleId]
+				});
+			}
+		}
+	},
+
+	// Firefox does not support responseHeaders condition, so we use webRequestBlocking instead
+	_addWebRequestRule: async function(tabId, ruleId) {
+		const redirectUrlBase = Zotero.getExtensionURL("browserAttachmentMonitor/browserAttachmentMonitor.html");
+	
+		const listener = (details) => {
+			let hasAttachmentDisposition = false;
+			let hasCorrectContentType = false;
+			
+			const contentType = details.responseHeaders.find(h => h.name.toLowerCase() === 'content-type')?.value;
+			const contentDisposition = details.responseHeaders.find(h => h.name.toLowerCase() === 'content-disposition')?.value;
+	
+			hasAttachmentDisposition = contentDisposition && contentDisposition.toLowerCase().includes('attachment');
+			hasCorrectContentType = contentType && (['application/pdf', 'application/epub+zip'].some(type => contentType.toLowerCase().includes(type)));
+	
+			if (hasAttachmentDisposition || hasCorrectContentType) {
+				const finalRedirectUrl = `${redirectUrlBase}#success=${details.url}`;
+				return { redirectUrl: finalRedirectUrl };
+			}
+	
+			// No redirect needed
+			return {}; 
+		};
+	
+		browser.webRequest.onHeadersReceived.addListener(
+			listener,
+			{ urls: ["<all_urls>"], 
+				tabId: tabId,
+				types: ["main_frame", "sub_frame"] 
+			},
+			["blocking", "responseHeaders"]
+		);
+		
+		// Store listener for removal
+		this._webRequestListeners.set(ruleId, listener);
+		Zotero.debug(`BrowserAttachmentMonitor: Added listener for tab ${tabId} with ruleId ${ruleId}`);
+	},
+	
+	_addDNRRule: async function(tabId, ruleId) {
+		const redirectUrl = Zotero.getExtensionURL("browserAttachmentMonitor/browserAttachmentMonitor.html#success=\\0");
+		await browser.declarativeNetRequest.updateSessionRules({
+			removeRuleIds: [ruleId],
+			addRules: [{
+				id: ruleId,
+				action: {
+					type: 'redirect',
+					redirect: {
+						regexSubstitution: redirectUrl
+					}
+				},
+				condition: {
+					// Need to explicitly specify main_frame, otherwise it is excluded
+					resourceTypes: ['main_frame', 'sub_frame'],
+					regexFilter: '.*',
+					tabIds: [tabId],
+					responseHeaders: [
+						{
+							header: 'content-disposition',
+							values: ['*attachment*']
+						},
+						{
+							header: 'content-type',
+							values: ['application/pdf', 'application/epub+zip']
+						}
+					]
+				}
+			}]
+		});
+	}
+};
+
+// Initialize when the module loads
+Zotero.BrowserAttachmentMonitor.init();

src/browserExt/browserAttachmentMonitor/browserAttachmentMonitor_inject.js
@@ -1,0 +1,19 @@
+(async function() {
+	window.addEventListener('load', async () => {
+		const match = /success=(.*)/.exec(window.location.hash);
+		const success = match ? match[1] : false;
+		try {
+			await browser.runtime.sendMessage({
+				type: 'attachment-monitor-loaded',
+				success
+			});
+		}
+		catch (e) { }
+		// Sanity wait for DNR to be updated in the background
+		await new Promise(resolve => setTimeout(resolve, 200));
+		const urlMatch = new URLSearchParams(window.location.hash.slice(1)).get('url');
+		if (urlMatch) {
+			window.location.href = decodeURIComponent(urlMatch);
+		}
+	});
+})(); 

commit 16e15123bac6dcd7b8c35ff2f9510757552426ef

Fix Firefox setting permanent DNR rules for some HTTP requests
Regression from (d2486c05)

See https://github.com/zotero/translate/issues/41

src/browserExt/webRequestIntercept.js
@@ -169,11 +169,21 @@ Zotero.WebRequestIntercept = {
 				initiatorDomains: [new URL(browser.runtime.getURL('')).hostname],
 			}
 		}];
+		// Keep the service worker alive while the rule is active
+		Zotero.Connector_Browser.setKeepServiceWorkerAlive(true);
 		try {
-			await browser.declarativeNetRequest.updateDynamicRules({
+			await browser.declarativeNetRequest.updateSessionRules({
 				removeRuleIds: rules.map(r => r.id),
 				addRules: rules,
 			});
+			// Automatically clean up the rule after 60 seconds in case the caller does not
+			setTimeout(async () => {
+				try {
+					await Zotero.WebRequestIntercept.removeRuleDNR(ruleID);
+				} catch (e) {
+					Zotero.logError(e);
+				}
+			}, 60000);
 			Zotero.debug(`HTTP: Added a DNR rule to change headers for ${url} to ${JSON.stringify(headers)}`);
 		}
 		catch (e) {
@@ -183,7 +193,8 @@ Zotero.WebRequestIntercept = {
 	},
 	
 	removeRuleDNR: async function(ruleId) {
-		return browser.declarativeNetRequest.updateDynamicRules({ removeRuleIds: [ruleId] });
+		Zotero.Connector_Browser.setKeepServiceWorkerAlive(false);
+		return browser.declarativeNetRequest.updateSessionRules({ removeRuleIds: [ruleId] });
 	}
 }
 

src/common/http.js
@@ -133,6 +133,7 @@ Zotero.HTTP = new function() {
 	
 	this._requestXHR = async function(method, url, options) {
 		var useContentXHR = false;
+		let DNRRuleID;
 		
 		// There is no reason to run xhr not from background page for web extensions since those
 		// requests send full browser cookies.
@@ -227,7 +228,7 @@ Zotero.HTTP = new function() {
 					return val;
 				});
 			if (replaceHeaders.length) {
-				await Zotero.WebRequestIntercept.replaceHeaders(url, replaceHeaders);
+				DNRRuleID = await Zotero.WebRequestIntercept.replaceHeaders(url, replaceHeaders);
 			}
 		}
 		Zotero.debug(`HTTP ${method} ${url}${logBody}`);
@@ -252,7 +253,8 @@ Zotero.HTTP = new function() {
 		
 		xmlhttp.send(options.body);
 		
-		return promise.then(function(xmlhttp) {
+		try {
+			xmlhttp = await promise;
 			if (options.debug && (xmlhttp.responseType === '' || xmlhttp.responseType === 'text')) {
 				Zotero.debug(`HTTP ${xmlhttp.status} ${xmlhttp.responseURL} response: ${xmlhttp.responseText}`);
 			}
@@ -267,7 +269,12 @@ Zotero.HTTP = new function() {
 				throw new Zotero.HTTP.StatusError(xmlhttp, url, typeof xmlhttp.responseText == 'string' ? xmlhttp.responseText : undefined);
 			}
 			return xmlhttp;
-		});
+		}
+		finally {
+			if (DNRRuleID) {
+				await Zotero.WebRequestIntercept.removeRuleDNR(DNRRuleID);
+			}
+		}
 	}
 	
 	this._requestFetch = async function(method, url, options) {

src/common/zotero.js
@@ -111,65 +111,27 @@ var Zotero = global.Zotero = new function() {
 	
 	this.migrate = async function() {
 		let lastVersion = Zotero.Prefs.get('lastVersion') || Zotero.version;
-		// If coming from a version before 5.0.24, reset the
-		// auto-associate setting for all existing proxies, since it wasn't being set properly for
-		// proxies imported from the client
-		if (Zotero.Utilities.semverCompare(lastVersion, "5.0.24") < 0 && Zotero.Prefs.get('proxies.clientChecked')) {
-			for (let proxy of Zotero.Proxies.proxies) {
-				proxy.autoAssociate = true;
-			}
-			Zotero.Proxies.storeProxies();
-		}
-		if (Zotero.Utilities.semverCompare(lastVersion, "5.0.32") < 0 && Zotero.Proxies.proxies.length > 1) {
-			let pairs = [];
-			// merge pairs of proxies with http and https protocols
-			for (let i = 0; i < Zotero.Proxies.proxies.length; i++) {
-				if (Zotero.Proxies.length == i+1) break;
-				let proxy1 = Zotero.Proxies.proxies[i];
-				let scheme = proxy1.scheme.replace('https', '').replace('http', '');
-				for (let j = i+1; j < Zotero.Proxies.proxies.length; j++) {
-					let proxy2 = Zotero.Proxies.proxies[j];
-					if (scheme == proxy2.scheme.replace('https', '').replace('http', '')) {
-						pairs.push([proxy1, proxy2]);
-						break;
-					}
-				}
-			}
-			for (let [proxy1, proxy2] of pairs) {
-				let json = proxy1.toJSON();
-				delete json.id;
-				let proxy = new Zotero.Proxy(json);
-				proxy.dotsToHyphens = true;
-				proxy.hosts = proxy1.hosts.concat(proxy2.hosts);
-				proxy.scheme = proxy.scheme.replace('http://', '').replace('https://', '');
-				Zotero.Proxies.remove(proxy1);
-				Zotero.Proxies.remove(proxy2);
-				Zotero.Proxies.save(proxy);
-			}
-			// remove protocols of single protocolless
-			for (let proxy of Zotero.Proxies.proxies) {
-				if (proxy.scheme.includes('://')) {
-					proxy.scheme = proxy.scheme.substr(proxy.scheme.indexOf('://')+3);
-					proxy.compileRegexp();
-					Zotero.Proxies.save(proxy);
-				}
-			}
-		}
-		// Botched dotsToHyphen pref migration to protocolless schemes in 5.0.32
-		if (Zotero.Utilities.semverCompare(lastVersion, "5.0.35") < 0) {
-			for (let proxy of Zotero.Proxies.proxies) {
-				if (proxy.scheme?.indexOf('%h') == 0) {
-					proxy.dotsToHyphens = true;
-				}
-			}
-			Zotero.Proxies.storeProxies();
-		}
 		// Skip first-use dialog for existing users when enabled for non-Firefox browsers
 		if (Zotero.Utilities.semverCompare(lastVersion, "5.0.87") < 0 && !this.isFirefox) {
 			Zotero.Prefs.set('firstUse', false);
 		}
 		if (Zotero.Utilities.semverCompare(lastVersion, "5.0.110") < 0) {
 			Zotero.Prefs.set('integration.googleDocs.useGoogleDocsAPI', false)
+		}
+		if (Zotero.Utilities.semverCompare(lastVersion, "5.0.168") < 0 && Zotero.isFirefox) {
+			// We were setting DNR header replacement rules on Firefox http.js without
+			// removing them, and this breaks translation sometimes
+			// See https://github.com/zotero/translate/issues/41
+			const rules = await browser.declarativeNetRequest.getDynamicRules({});
+			let ruleIDs = [];
+			for (const rule of rules) {
+				if (rule.action.type === 'modifyHeaders') {
+					ruleIDs.push(rule.id);
+				}
+			}
+			if (ruleIDs.length) {
+				await browser.declarativeNetRequest.updateDynamicRules({ removeRuleIds: ruleIDs });
+			}
 		}
 		Zotero.Prefs.set('lastVersion', Zotero.version);
 	};
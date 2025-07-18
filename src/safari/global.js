4bdd570a5c50f34387be9a75d4fe7adbea3b8c23
Adomas Venčkauskas <adomas.ven@gmail.com>
2025-07-18 09:57:17 +0300

    New icons: Fix PDF icon

--- src/safari/global.js
+++ src/safari/global.js
@@ -324,7 +324,7 @@ Zotero.Connector_Browser = new function() {
 	}
 
 	function _showPDFIcon() {
-		let image = "images/toolbar/attachment-pdf.png";
+		let image = "images/toolbar/pdf.png";
 		let tooltip = "Save to Zotero (PDF)";
 		return [image, tooltip]
 	}

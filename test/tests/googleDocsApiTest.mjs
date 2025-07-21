1dd897e55e07d3cac53e8e95bc1d20cf2489a496
Adomas Venčkauskas <adomas.ven@gmail.com>
2025-07-21 14:18:02 +0300

    Report the count of 500 errors received for the same document
    Updates Google Docs submodule

--- test/tests/googleDocsApiTest.mjs
+++ test/tests/googleDocsApiTest.mjs
@@ -63,6 +63,92 @@ describe("Zotero.GoogleDocs.API", function() {
 			assert.isArray(reportCallArgs);
 			assert.include(reportCallArgs[1], 'zotero.org');
 			assert.include(reportCallArgs[2].body, 'googleDocsV2APIError');
+			assert.include(reportCallArgs[2].body, `count%22%3A` + 1);
+		});
+
+		it('should report multiple 500 errors with the count of the errors for the same document', async function() {
+			let reportCallArgs = await background(async function() {
+				let authStub, httpStub;
+				let reportCallArgs;
+				try {
+					// Stub auth headers
+					authStub = sinon.stub(Zotero.GoogleDocs.API, 'getAuthHeaders').resolves({'Authorization': 'Bearer test'});
+					
+					// Stub HTTP requests
+					httpStub = sinon.stub(Zotero.HTTP, 'request').callThrough();
+					
+					// First call (to docs.googleapis.com) throws 500 error
+					httpStub.withArgs('GET', 'https://docs.googleapis.com/v1/documents/test-doc-id2?includeTabsContent=true').rejects(new Zotero.HTTP.StatusError({status: 500, responseText: 'Internal Server Error'}));
+					// Second call (to zotero.org) returns OK
+					httpStub.withArgs('POST', 'https://repo.zotero.org/repo/report').callsFake((...args) => {
+						reportCallArgs = args;
+					});
+
+					for (let i = 0; i < 5; i++) {
+						try {
+							await Zotero.GoogleDocs.API.getDocument('test-doc-id2');
+						} catch (e) {
+							Zotero.debug(e);
+						}
+					}
+					
+					// Return the arguments from the second call
+					return reportCallArgs;
+				}
+				finally {
+					authStub?.restore();
+					httpStub?.restore();
+				}
+			});
+			
+			assert.isArray(reportCallArgs);
+			assert.include(reportCallArgs[1], 'zotero.org');
+			assert.include(reportCallArgs[2].body, 'googleDocsV2APIError');
+			assert.include(reportCallArgs[2].body, `count%22%3A` + 5);
+		});
+
+		it('if a document retrieval succeeds, the 500 count is reset', async function() {
+			let reportCallArgs = await background(async function() {
+				let authStub, httpStub;
+				let reportCallArgs;
+				try {
+					// Stub auth headers
+					authStub = sinon.stub(Zotero.GoogleDocs.API, 'getAuthHeaders').resolves({'Authorization': 'Bearer test'});
+					
+					// Stub HTTP requests
+					httpStub = sinon.stub(Zotero.HTTP, 'request').callThrough();
+					
+					// Calls (to docs.googleapis.com) throws 500 error
+					httpStub.withArgs('GET', 'https://docs.googleapis.com/v1/documents/test-doc-id2?includeTabsContent=true')
+						.rejects(new Zotero.HTTP.StatusError({status: 500, responseText: 'Internal Server Error'}));
+					// Success on second call (reset count)
+					httpStub.withArgs('GET', 'https://docs.googleapis.com/v1/documents/test-doc-id2?includeTabsContent=true')
+						.onSecondCall().resolves({responseText: '{"documentId": "test-doc-id2"}', status: 200});
+					httpStub.withArgs('POST', 'https://repo.zotero.org/repo/report').callsFake((...args) => {
+						reportCallArgs = args;
+					});
+
+					for (let i = 0; i < 3; i++) {
+						try {
+							await Zotero.GoogleDocs.API.getDocument('test-doc-id2');
+						} catch (e) {
+							Zotero.debug(e);
+						}
+					}
+					
+					// Return the arguments from the second call
+					return reportCallArgs;
+				}
+				finally {
+					authStub?.restore();
+					httpStub?.restore();
+				}
+			});
+			
+			assert.isArray(reportCallArgs);
+			assert.include(reportCallArgs[1], 'zotero.org');
+			assert.include(reportCallArgs[2].body, 'googleDocsV2APIError');
+			assert.include(reportCallArgs[2].body, `count%22%3A` + 1);
 		});
 	});
 }); 

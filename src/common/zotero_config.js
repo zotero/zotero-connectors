ea060a0aa2fea1267049b5fc880e53aa6c915eeb
Abe Jellinek <1770299+AbeJellinek@users.noreply.github.com>
2026-02-03 16:41:23 -0500

    Allow translator CI to prevent fetches from Zotero

--- src/common/zotero_config.js
+++ src/common/zotero_config.js
@@ -31,6 +31,7 @@ const ZOTERO_CONFIG = {
 	REPOSITORY_CHECK_INTERVAL: 86400, // 24 hours
 	REPOSITORY_RETRY_INTERVAL: 3600, // 1 hour
 	REPOSITORY_CHANNEL: 'trunk',
+	ALWAYS_FETCH_FROM_REPOSITORY: false,
 	BASE_URI: 'https://zotero.org/',
 	WWW_BASE_URL: 'https://www.zotero.org/',
 	CLIENT_DOWNLOAD_URL: 'https://www.zotero.org/download',
